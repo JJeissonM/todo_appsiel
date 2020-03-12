aa6de57812ee4219312b87149c97852cfbebc6bc
Camilo <c_colon10@hotmail.com>
2020-03-12 13:18:16 -0500

    Página web(Detalles version landing page ) 12 de marzo 2020 Camilo Colón

--- appsiel/resources/views/components/web/footer.blade.php
+++ appsiel/resources/views/components/web/footer.blade.php
@@ -12,6 +12,8 @@
         }
     </style>
 
+
+    <div style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M0.00,49.98 C334.36,229.44 308.96,-42.92 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: {{$footer->background}};"></path></svg></div>
     <div class="footerarea">
         <div class="container">
             <div class="row">
