21bfbbeed4c64f66375d661f906cd4901e272610
Adalberto Pérez Oliveros <adalberto.perez@appsiel.com.co>
2020-03-19 14:39:09 -0500

    cambio ModeloController

--- appsiel/app/Http/contratotransporte_routes.php
+++ appsiel/app/Http/contratotransporte_routes.php
@@ -1,4 +1,9 @@
 <?php 
 
 //      CONTRATO DE TRANSPORTE (FORMATO UNICO DE EXTRACTO DE CONTRATO DE TRANSPORTE)
+
+Route::resource('cte_contratos', 'ContratoTransporte\ContratoTransporteController');
+
+Route::resource('cte_plantillas', 'ContratoTransporte\ContratoTransporteController');
+
 Route::resource('contratos_transporte', 'ContratoTransporte\ContratoTransporteController');
--- appsiel/resources/views/layouts/menu-item.blade.php
+++ appsiel/resources/views/layouts/menu-item.blade.php
@@ -11,16 +11,15 @@
     @endcan
     <ul class="dropdown-menu sub-menu">
         @foreach ($item['submenu'] as $submenu)
-        @if ($submenu['submenu'] == [])
-        @can($item['name'])
-        <li>
-            <a href="{{ url($submenu['url'].'?id='.$submenu['core_app_id'].'&id_modelo='.$submenu['modelo_id']) }}"> {{ $submenu['descripcion'] }} </a>
-            <!-- <a href="{ { url($submenu['url'].'?id='.$submenu['core_app_id'].'&id_modelo='.$submenu['modelo_id']) }}" target="_blank"> <i class="fa fa-external-link"></i> </a> -->
-        </li>
-        @endcan
-        @else
-        @include('layouts.menu-item', [ 'item' => $submenu ])
-        @endif
+            @if ($submenu['submenu'] == [])
+                @can($submenu['name'])
+                    <li>
+                        <a href="{{ url($submenu['url'].'?id='.$submenu['core_app_id'].'&id_modelo='.$submenu['modelo_id']) }}"> {{ $submenu['descripcion'] }} </a>
+                    </li>
+                @endcan
+            @else
+                @include('layouts.menu-item', [ 'item' => $submenu ])
+            @endif
         @endforeach
     </ul>
 </li>
