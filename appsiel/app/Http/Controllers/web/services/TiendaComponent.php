bfc36395c725910423bca7199e23a7984e039ed9
Camilo <c_colon10@hotmail.com>
2020-03-26 08:17:30 -0500

    Página web(Tienda Online ) 26 de marzo 2020 Camilo Colón

--- appsiel/app/Http/Controllers/web/services/TiendaComponent.php
+++ appsiel/app/Http/Controllers/web/services/TiendaComponent.php
@@ -18,22 +18,18 @@ class TiendaComponent implements IDrawComponent
 
     function DrawComponent()
     {
-        $pedido = Pedidoweb::where('widget_id', $this->widget)->first();
-        $items = null;
-        if ($pedido != null) {
-            $items = InvProducto::where([['mostrar_en_pagina_web', 1]])->orderBy('created_at', 'DESC')->get();
-            if (count($items) > 0) {
-                foreach ($items as $i) {
-                    $i->grupo = "---";
-                    $g = InvGrupo::find($i->inv_grupo_id);
-                    if ($g != null) {
-                        $i->grupo = $g->descripcion;
-                    }
+        $items = InvProducto::where([['mostrar_en_pagina_web', 1]])->orderBy('created_at', 'DESC')->get();
+        if (count($items) > 0) {
+            foreach ($items as $i) {
+                $i->grupo = "---";
+                $g = InvGrupo::find($i->inv_grupo_id);
+                if ($g != null) {
+                    $i->grupo = $g->descripcion;
                 }
+                $items = null;
             }
         }
-
-        return Form::tienda($items, $pedido);
+        return Form::tienda($items);
     }
 
     function viewComponent()
