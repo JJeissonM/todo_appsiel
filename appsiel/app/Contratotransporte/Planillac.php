e1b058be243ef7b390820aa4be61e1a5817c71c2
JJeissonM <jeisson.mandon@appsiel.com.co>
2020-06-03 07:16:05 -0500

    Cambios Transporcol (3 de Junio 2020) Jeisson

--- appsiel/app/Contratotransporte/Planillac.php
+++ appsiel/app/Contratotransporte/Planillac.php
@@ -6,23 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Planillac extends Model
 {
-	// Este es el documento FUEC como tal (el PDF)
+    // Este es el documento FUEC como tal (el PDF)
     protected $table = 'cte_planillacs';
     protected $fillable = ['id', 'contrato_id', 'razon_social', 'nit', 'convenio', 'plantilla_id', 'created_at', 'updated_at'];
 
     public static function opciones_campo_select()
     {
         $opciones = Planillac::leftJoin('cte_contratos', 'cte_contratos.id', '=', 'cte_planillacs.contrato_id')
-                            ->leftJoin('cte_plantillas', 'cte_plantillas.id', '=', 'cte_planillacs.plantilla_id')
-                            ->select('cte_planillacs.id','cte_contratos.codigo AS contrato_codigo','cte_plantillas.titulo AS plantilla_titulo')
-                            ->get();
+            ->leftJoin('cte_plantillas', 'cte_plantillas.id', '=', 'cte_planillacs.plantilla_id')
+            ->select('cte_planillacs.id', 'cte_contratos.codigo AS contrato_codigo', 'cte_plantillas.titulo AS plantilla_titulo')
+            ->get();
 
-        $vec['']='';
-        foreach ($opciones as $opcion)
-        {
-            $vec[$opcion->id] = $opcion->contrato_codigo.' > '.$opcion->plantilla_titulo;
+        $vec[''] = '';
+        foreach ($opciones as $opcion) {
+            $vec[$opcion->id] = $opcion->contrato_codigo . ' > ' . $opcion->plantilla_titulo;
         }
 
         return $vec;
+    }
+
+    public function planillaconductors()
+    {
+        return $this->hasMany(Planillaconductor::class);
+    }
+
+    public function contrato()
+    {
+        return $this->belongsTo(Contrato::class);
     }
 }
--- appsiel/app/Contratotransporte/Planillaconductor.php
+++ appsiel/app/Contratotransporte/Planillaconductor.php
@@ -8,4 +8,15 @@ class Planillaconductor extends Model
 {
     protected $table = 'cte_planillaconductors';
     protected $fillable = ['id', 'conductor_id', 'planillac_id', 'created_at', 'updated_at'];
+
+
+    public function conductor()
+    {
+        return $this->belongsTo(Conductor::class);
+    }
+
+    public function planillac()
+    {
+        return $this->belongsTo(Planillac::class);
+    }
 }
