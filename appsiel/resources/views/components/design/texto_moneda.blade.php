37c3c67b81fd99c6da5a9ef576eaf0d641fcb69a
Adalberto Perez <adalberto.perez@appsiel.com.co>
2020-07-01 09:43:13 -0500

    grafico mapa riqueza contabilidad

--- appsiel/resources/views/components/design/texto_moneda.blade.php
+++ appsiel/resources/views/components/design/texto_moneda.blade.php
@@ -1,5 +1,10 @@
 <table style="width: 100%; margin: 0px;">
 	<tr>
+		@if( !is_null($lbl) )
+			<td width="35%" style="border: 0px;">
+				<strong> {{ $lbl }} </strong>
+			</td>
+		@endif
 		<td width="5px" style="border: 0px;">
 			$
 		</td>
