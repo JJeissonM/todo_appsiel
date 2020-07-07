b38c908215ffaaceca5677f89339f5820cea6047
colonca1999@gmail.com <camilo14>
2020-07-07 12:00:55 -0500

    Tienda online pedidos

--- appsiel/app/Http/Controllers/Core/TransaccionController.php
+++ appsiel/app/Http/Controllers/Core/TransaccionController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers\Core;
 
+use App\Contabilidad\Impuesto;
+use App\Ventas\ListaDctoDetalle;
+use App\Ventas\ListaPrecioDetalle;
+use App\Ventas\VtasDocRegistro;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -167,7 +171,14 @@ class TransaccionController extends Controller
     public function crear_encabezado_documento(Request $request, $modelo_id)
     {
         $request['creado_por'] = Auth::user()->email;
-        $request['valor_total'] = 10;
+        $lineas_registros = json_decode($request->lineas_registros);
+        $cantidad_registros = count($lineas_registros);
+        $total_documento = 0;
+        for ($i = 0; $i < $cantidad_registros; $i++)
+        {
+            $total_documento += $lineas_registros[$i]->precio_total;
+        } // Fin por cada registro
+        $request['valor_total'] = $total_documento;
         return CrudController::crear_nuevo_registro( $request, $modelo_id );
     }
 
