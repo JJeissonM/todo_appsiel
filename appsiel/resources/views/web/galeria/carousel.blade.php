27a86f6fbf393b5c70e1612d38223e3dec03f505
Adalberto Perez <adalberto.perez@appsiel.com.co>
2020-10-17 13:22:32 -0500

    quitar auto slide from carousel

--- appsiel/resources/views/web/galeria/carousel.blade.php
+++ appsiel/resources/views/web/galeria/carousel.blade.php
@@ -9,13 +9,8 @@
   </p>
 </div>
 
-<div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="3000">
-  <!-- Indicators 
-  <ol class="carousel-indicators">
-    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-    <li data-target="#myCarousel" data-slide-to="1"></li>
-    <li data-target="#myCarousel" data-slide-to="2"></li>
-  </ol>-->
+<!-- <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="3000"> -->
+<div id="myCarousel" class="carousel slide" data-interval="false">
 
   <!-- Wrapper for slides -->
   <div class="carousel-inner">
