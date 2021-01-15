74cba15c9870a978039a91683f8f80b3587161da
JJeissonM <jeisson.mandon@appsiel.com.co>
2021-01-15 08:21:48 -0500

    Actualización 15 Enero 2021 Jeisson

---
+++ appsiel/config/calificaciones.php
@@ -0,0 +1,13 @@
+<?php
+ return array (
+  'titulo' => 'Configuración de Calificaciones',
+  'permitir_calificaciones_sin_logros' => 'Si',
+  'manejar_preinformes_academicos' => 'No',
+  'colegio_maneja_metas' => 'Si',
+  'etiqueta_calificacion_boletines' => 'numero_y_letras',
+  'permitir_imprimir_boletin_a_estudiantes' => 'No',
+  'formato_boletin_default' => 'pdf_boletines_3',
+  'estudiante_revisar_guia_academicas' => 'Si',
+  'estudiante_activar_foros_discucion' => 'No',
+  'url_id_modelo' => '0',
+) ;
---
+++ appsiel/config/nomina.php
@@ -0,0 +1,14 @@
+<?php
+ return array (
+  'titulo' => '<h3>Configuraciones generales <br> <small style="color: red;"> Si no está completamente seguro, no modifique ningún valor de este formulario. Consulte con el administrador del sistema. </small></h3>',
+  'SMMLV' => '877803',
+  'horas_laborales' => '240',
+  'sabado_es_dia_habil' => '1',
+  'concepto_salario_integral' => '2',
+  'concepto_salud_obligatoria' => '64',
+  'pago_salario_completo_en_incapacidades' => '1',
+  'id_concepto_pagar_empresa_en_incapacidades' => '71',
+  'concepto_a_pagar_pasante_sena' => '77',
+  'porcentaje_liquidacon_pasante_sena' => '75',
+  'url_id_modelo' => '0',
+) ;
