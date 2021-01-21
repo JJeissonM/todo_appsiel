cf9d1ba1b70ac713f2f69562732a1e2f2cc1296f
Adalberto Perez <adalberto.perez@appsiel.com.co>
2021-01-21 12:34:57 -0500

    Cambios 21ene2021 1234pm

---
+++ appsiel/config - copia/contrato_transporte.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'numero_territorial' => '220',
+    'resolucion_habilitacion' => '0019',
+    'anio_creacion_empresa' => '02'
+];
---
+++ appsiel/config - copia/tesoreria.php
@@ -0,0 +1,11 @@
+<?php
+ return array (
+  'titulo' => '<h3>Configuraciones generales <br> <small style="color: red;"> Si no está completamente seguro, no modifique ningún valor de este formulario. Consulte con el administrador del sistema. </small></h3>',
+  'motivo_recibo_caja_id' => '1',
+  'motivo_comprobante_egresos_id' => '5',
+  'formato_libreta_pago_defecto' => 'pdf_libreta',
+  'recaudo_tipo_transaccion_id' => '32',
+  'recaudo_tipo_doc_app_id' => '3',
+  'recaudo_motivo_id' => '21',
+  'url_id_modelo' => '0',
+) ;
