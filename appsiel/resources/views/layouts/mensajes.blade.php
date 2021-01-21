cd1e378c93b9bf95a404ee3749acc6e240f10a6f
JJeissonM <jeisson.mandon@appsiel.com.co>
2021-01-21 12:29:11 -0500

    Corrección Alert Width 21 Enero de 2021 Jeisson

--- appsiel/resources/views/layouts/mensajes.blade.php
+++ appsiel/resources/views/layouts/mensajes.blade.php
@@ -1,5 +1,5 @@
 @if(Session::has('flash_message'))
-	<div class="container" style="clear: both">
+	<div class="container" style="clear: both; width: auto;">
 		<div class="alert alert-success alert-dismissible">
 			<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
 			<em> {!! session('flash_message') !!}</em>
@@ -8,7 +8,7 @@
 @endif
 
 @if(Session::has('mensaje_error'))
-	<div class="container" style="clear: both">      
+	<div class="container" style="clear: both; width: auto;">      
 		<div class="alert alert-danger alert-dismissible">
 			<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
 			<em> {!! session('mensaje_error') !!}</em>
