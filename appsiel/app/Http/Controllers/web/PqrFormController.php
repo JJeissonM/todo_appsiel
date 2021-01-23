a107fd6fa2c49b4fd16c6b2003f1abc6bdc784e2
Ricardo Garces <ricardo.garces@appsiel.com.co>
2021-01-23 12:09:22 -0500

    23 Enero - campos Fuentes y fondo PQR

--- appsiel/app/Http/Controllers/web/PqrFormController.php
+++ appsiel/app/Http/Controllers/web/PqrFormController.php
@@ -19,61 +19,106 @@ use App\web\PqrForm;
 
 class PqrFormController extends Controller
 {
-    
+
     public function store(Request $request)
     {
-        $registro = PqrForm::create( $request->all() );
+        if ($request->tipo_fondo == '') {
+            return redirect(url('seccion/' . $request->widget_id) . $request->variables_url)->with('mensaje_error', 'Debe indicar el tipo de fondo a usar en el componente.');
+        }
 
-        return redirect( 'seccion/' . $request->widget_id . '?id=' . $request->url_id)->with('flash_message', 'Sección almacenada correctamente.');
+        $registro = PqrForm::create($request->all());
+
+        if ($request->tipo_fondo == 'IMAGEN') {
+            //el fondo es una imagen
+            $file = $request->file('fondo');
+            $name = time() . $file->getClientOriginalName();
+            $filename = "img/" . $name;
+            $flag = file_put_contents($filename, file_get_contents($file->getRealPath()), LOCK_EX);
+            if ($flag !== false) {
+                $registro->fondo = $filename;
+            } else {
+                $message = 'Error inesperado al intentar guardar la imagen de fondo, por favor intente nuevamente mas tarde';
+                return redirect()->back()->withInput($request->input())
+                    ->with('mensaje_error', $message);
+            }
+        }
+
+        return redirect('seccion/' . $request->widget_id . '?id=' . $request->url_id)->with('flash_message', 'Sección almacenada correctamente.');
     }
 
     public function update(Request $request, $id)
     {
-        if ( $id == 'enviar' )
-        {
-            return $this->enviar_formulario( $request );
+        if ($id == 'enviar') {
+            return $this->enviar_formulario($request);
         }
 
-        $registro = PqrForm::find( $id );
-        $registro->fill( $request->all() );
+        $registro = PqrForm::find($id);
+        $tipo_fondo = $registro->tipo_fondo;
+        $registro->configuracionfuente_id = $request->configuracionfuente_id;
+        if ($request->tipo_fondo == '') {
+            $registro->tipo_fondo = $tipo_fondo;
+        }
+        if ($request->tipo_fondo != '') {
+            if ($request->tipo_fondo == 'IMAGEN') {
+                if (isset($request->fondo)) {
+                    //el fondo es una imagen
+                    $file = $request->file('fondo');
+                    $name = time() . $file->getClientOriginalName();
+                    $filename = "img/" . $name;
+                    $flag = file_put_contents($filename, file_get_contents($file->getRealPath()), LOCK_EX);
+                    if ($flag !== false) {
+                        $registro->fondo = $filename;
+                        $registro->tipo_fondo = 'IMAGEN';
+                        $registro->repetir = $request->repetir;
+                        $registro->direccion = $request->direccion;
+                    } else {
+                        $message = 'Error inesperado al intentar guardar la imagen de fondo, por favor intente nuevamente mas tarde';
+                        return redirect()->back()->withInput($request->input())
+                            ->with('mensaje_error', $message);
+                    }
+                }
+            } else {
+                $registro->fondo = $request->fondo;
+                $registro->tipo_fondo = "COLOR";
+            }
+        }
         $registro->save();
 
-        return redirect( 'seccion/' . $request->widget_id . '?id=' . $request->url_id)->with('flash_message', 'Sección actualizada correctamente.');
-
+        return redirect('seccion/' . $request->widget_id . '?id=' . $request->url_id)->with('flash_message', 'Sección actualizada correctamente.');
     }
 
 
 
-    public function enviar_formulario( Request $request )
+    public function enviar_formulario(Request $request)
     {
         $empresa = Empresa::find(1);
 
         // Email interno. Debe estar creado en Hostinger
-        $email_interno = 'info@'.substr( url('/'), 7);
+        $email_interno = 'info@' . substr(url('/'), 7);
 
         // Datos requeridos por hostinger
-        $from = "Pagina Web <".$email_interno."> \r\n";
-        $headers = "From:" . $from." \r\n";
+        $from = "Pagina Web <" . $email_interno . "> \r\n";
+        $headers = "From:" . $from . " \r\n";
         $to = $request->email_recepcion;
         $subject = "Comentario desde página Web";
 
-        
+
         // El mensaje
-        $formulario = PqrForm::where( 'widget_id', $request->widget_id )->get()->first();
-        $campos_mostrar = json_decode( $formulario->campos_mostrar );        
+        $formulario = PqrForm::where('widget_id', $request->widget_id)->get()->first();
+        $campos_mostrar = json_decode($formulario->campos_mostrar);
         $campos = [];
         foreach ($campos_mostrar as $key => $value) {
-            $el_campo = Campo::find( $key );
+            $el_campo = Campo::find($key);
             $variable = $el_campo->name;
-            $campos[] = [ $el_campo->descripcion, $request->$variable ];
+            $campos[] = [$el_campo->descripcion, $request->$variable];
         }
-        $cuerpo_mensaje = View::make('web.formularios.cuerpo_mensaje',compact('request','empresa','campos'))->render();
+        $cuerpo_mensaje = View::make('web.formularios.cuerpo_mensaje', compact('request', 'empresa', 'campos'))->render();
 
 
         // Encabezados
         $headers .= "MIME-Version: 1.0\r\n";
         $headers .= "Content-Type: multipart/mixed; boundary=\"=A=G=R=O=\"\r\n\r\n";
-        
+
         $message = "--=A=G=R=O=\r\n";
         $message .= "Content-type:text/html; charset=utf-8\r\n";
         $message .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
@@ -83,10 +128,8 @@ class PqrFormController extends Controller
         // Manjeo de archivos adjuntos
         $archivos_enviados = $request->file();
 
-        if( !empty( $archivos_enviados ) )
-        {
-            foreach ($archivos_enviados as $key => $value)
-            {
+        if (!empty($archivos_enviados)) {
+            foreach ($archivos_enviados as $key => $value) {
 
                 // ALMACENAR TEMPORALMENTE EL ARCHIVO EN DISCO
                 $archivo = $request->file($key);
@@ -95,14 +138,14 @@ class PqrFormController extends Controller
                 $nombrearchivo = str_slug($archivo->getClientOriginalName()) . '-' . uniqid() . '.' . $extension;
 
                 // Guardar en disco
-                Storage::put( 'pdf_email/' . $nombrearchivo, file_get_contents($archivo->getRealPath()));
+                Storage::put('pdf_email/' . $nombrearchivo, file_get_contents($archivo->getRealPath()));
 
 
 
                 // LEER EL ARCHIVO Y ADJUNTARLO AL CUERPO DEL MENSAJE
 
-                $url = Storage::getAdapter()->applyPathPrefix('pdf_email/'.$nombrearchivo);
-                $file = chunk_split(base64_encode(file_get_contents( $url )));
+                $url = Storage::getAdapter()->applyPathPrefix('pdf_email/' . $nombrearchivo);
+                $file = chunk_split(base64_encode(file_get_contents($url)));
 
                 $message .= "--=A=G=R=O=\r\n";
                 $message .= "Content-Type: application/octet-stream; name=\"" . $nombrearchivo . "\"\r\n";
@@ -110,14 +153,13 @@ class PqrFormController extends Controller
                 $message .= "Content-Disposition: attachment; filename=\"" . $nombrearchivo . "\"\r\n\r\n";
                 $message .= $file . "\r\n\r\n";
                 $message .= "--=A=G=R=O=--";
-            }                
+            }
         }
 
         //dd( [ $to, $subject, $message, $headers ] );
 
         //if(true)
-        if ( mail( $to, $subject, $message, $headers) )
-        {
+        if (mail($to, $subject, $message, $headers)) {
             $tipo_msj = 'flash_message';
             $alerta = '<strong>¡El mensaje se ha enviado correctamente!</strong> Nos comunicaremos con usted los más pronto posible.';
         } else {
@@ -125,7 +167,6 @@ class PqrFormController extends Controller
             $alerta = '<strong>¡El mensaje no pudo ser enviado!</strong> Por favor intente nuevamente. Si el problema persiste, intente más tarde.';
         }
 
-        return redirect( $request->pagina_slug )->with( $tipo_msj, $alerta);
+        return redirect($request->pagina_slug)->with($tipo_msj, $alerta);
     }
-
 }
--- appsiel/resources/views/components/web/pqr.blade.php
+++ appsiel/resources/views/components/web/pqr.blade.php
@@ -1,29 +1,62 @@
 <link rel="stylesheet" href="{{asset('css/sweetAlert2.min.css')}}">
-<div class="container-wrapper">
+
+<style type="text/css">
+    #pqrform {
+
+        <?php if ($registro !=null) {
+            if ($registro->tipo_fondo=='COLOR') {
+                echo "background-color: ". $registro->fondo . ";";
+            } else {
+                ?>background: url('{{$registro->fondo}}') {{$registro->repetir}} center {{$registro->direccion}};
+                <?php
+            }
+        }
+
+        ?>
+    }
+
+    .pqrform-font {
+        @if( !is_null($registro)) @if( !is_null($registro->configuracionfuente)) font-family: <?php echo $registro->configuracionfuente->fuente->font;
+        ?> !important;
+        @endif @endif
+    }
+
+    #pqrform p{
+        text-align: center;
+    }
+</style>
+
+<div id="pqrform" class="container-wrapper pqrform-font">
     <div class="container">
         <div class="row" style="border-radius: 10px; background-color: white;">
             <div class="col-md-12">
 
                 @include('layouts.mensajes')
 
-                @if( !is_null($registro) )
-                    
+                @if( !is_null($registro) )            
+
+                <div class="col-sm-9">
                     {!! $registro->contenido_encabezado !!}
+                 </div> 
 
-                    {{ Form::open(['url'=>'pqr_form/enviar','id'=>'formulario_pqr','method'=>'PUT','files' => true]) }}
-                        
-                        {!! $registro->get_lista_campos() !!}
+                {{ Form::open(['url'=>'pqr_form/enviar','id'=>'formulario_pqr','method'=>'PUT','files' => true]) }}
 
-                        {{ Form::hidden('email_recepcion', $registro->parametros) }}
-                        {{ Form::hidden('fecha_hora', date('Y-m-d h:i a') ) }}
-                        {{ Form::hidden('widget_id', $registro->widget_id ) }}
-                        {{ Form::hidden('pagina_slug', $pagina->slug ) }}
+                {!! $registro->get_lista_campos() !!}
 
-                        <input class="btn btn-primary" type="submit" name="btn_enviar" value="Enviar mensaje">
+                {{ Form::hidden('email_recepcion', $registro->parametros) }}
+                {{ Form::hidden('fecha_hora', date('Y-m-d h:i a') ) }}
+                {{ Form::hidden('widget_id', $registro->widget_id ) }}
+                {{ Form::hidden('pagina_slug', $pagina->slug ) }}
 
-                    {{ Form::close() }}
+                <div class="col-sm-9">
+                   <input class="btn btn-primary form-control" type="submit" name="btn_enviar" value="Enviar mensaje"> 
+                </div>                
 
+                {{ Form::close() }}
+
+                <div class="col-sm-9">
                     {!! $registro->contenido_pie_formulario !!}
+                 </div>                 
 
                 @endif
 
@@ -34,8 +67,7 @@
 
 <script src="{{asset('js/sweetAlert2.min.js')}}"></script>
 <script type="text/javascript">
-        
-        document.getElementById('email').attributes[4].value = "email";
+    document.getElementById('email').attributes[4].value = "email";
 
         function enviar_form( btn )
         {
