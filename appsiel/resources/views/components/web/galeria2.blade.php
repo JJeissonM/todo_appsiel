47d8caa5d6b8b8e7d02a1c8003b7c190558fd5fa
Ricardo Garces <ricardo.garces@appsiel.com.co>
2021-01-23 12:06:17 -0500

    24 Enero - Correcciones galeria

--- appsiel/resources/views/components/web/galeria2.blade.php
+++ appsiel/resources/views/components/web/galeria2.blade.php
@@ -2,7 +2,7 @@
 
         #portfolio {
             position: relative;
-            z-index: 80 !important;
+            /*z-index: 80 !important;*/
             padding: 100px 0 75px;
 
             <?php
@@ -57,6 +57,66 @@
           /*background-image: linear-gradient(45deg, #3023AE 0%, #f09 100%);*/
         }
 
+        .abrir_modal{
+          border: 1px solid #e6e6e6 ;
+          box-shadow: 10px 10px 5px 0px #e6e6e6;
+        }
+
+        .abrir_modal:hover{
+          transform: rotate(4deg); /* Standard syntax */
+        }
+
+
+        .modal{
+          width: 100vw;
+          right: unset;
+          bottom: unset;
+          height: 100vh;
+        }
+
+        .show{
+          display: flex !important;
+        }
+
+        .modal-dialog-centered{
+          width: 100vw;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        .modal-dialog{
+          max-width: 100%;
+          margin: 1rem 0;
+        }
+
+        .modal-content{
+          border-radius: 0;
+          border: none;
+          box-shadow: 
+          box-shadow: 0 5px 15px rgba(0,0,0,.5);
+        }
+
+        @media (min-width: 576px){
+          .modal-dialog {
+              max-width: 100%;
+              margin: 30px auto;
+          }
+        }
+
+        .img-fluid{
+          max-height: 80vh;
+        }
+
+        .carousel-control-prev{
+          justify-content: flex-end;
+          background: linear-gradient(to Left, rgba(255,255,255,0) 50%, rgba(219,219,219,1) 100%);
+        }
+        .carousel-control-next{
+          justify-content: flex-end;
+          background: linear-gradient(to right, rgba(255,255,255,0) 50%, rgba(219,219,219,1) 100%);
+        } 
+
 
 </style>
 
@@ -90,10 +150,11 @@
 
     <!-- The Modal -->
     <div class="modal" id="myModal" data-url_busqueda="{{ url('galeria_ver_album_carousel') }}">
-      <div class="modal-dialog modal-lg modal-dialog-centered" style="height: 100% !important;">
+      <!--<div class="modal-dialog modal-lg modal-dialog-centered" style="height: 100% !important;">-->
+        <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
           
-          <div style="text-align: center; position: absolute; z-index: 9999; right: 50%; padding-top: 20px; color: #ddd;">
+          <div style="text-align: center; position: absolute; z-index: 9999; right: 0; color: #ddd;">
             <button type="button" class="btn_cerrar" data-dismiss="modal">&times;</button>  
           </div>          
           
