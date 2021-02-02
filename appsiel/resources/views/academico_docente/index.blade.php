72ac052168e632e7f012f36b85011d030690b893
JJeissonM <jeisson.mandon@appsiel.com.co>
2021-02-02 13:12:29 -0500

    Vista Docente Febrero 2 de 2021 Jeisson

--- appsiel/resources/views/academico_docente/index.blade.php
+++ appsiel/resources/views/academico_docente/index.blade.php
@@ -18,25 +18,44 @@
 			</div>
 			<div class="panel-body">
 				@if( !is_null($listado) )
+				<?php
+				$modelo_preinforme_academico_id = 192;
+				?>
 
 				<div class="col-md-12 botones-gmail">
 					<!-- ASISTENCIA A CLASE -->
 					<a class="btn-gmail" onclick="asistencia()" title="Asistencia"><i class="fa fa-list"></i></a>
+					<!-- PLANES DE CLASES Y GUÍAS ACADÉMICAS -->
 					<a class="btn-gmail" onclick="planClaseCrear()" title="Ingresar Plan de Clases"><i class="fa fa-plus"></i></a>
 					<a class="btn-gmail" href="{{url('web')}}?id={{Input::get('id')}}&id_modelo={{$modelo_plan_clases_id}}" title="Consultar Plan de clases"><i class="fa fa-search"></i></a>
 					<a class="btn-gmail" onclick="guiaCrear()" title="Ingresar Guía Académica"><i class="fa fa-book"></i></a>
 					<a class="btn-gmail" href="{{url('web')}}?id={{Input::get('id')}}&id_modelo={{$modelo_guia_academica_id}}" title="Consultar Guía Académica"><i class="fa fa-search"></i></a>
-					<a class="btn-gmail" href="{{url('web')}}?id={{Input::get('id')}}&id_modelo={{$modelo_guia_academica_id}}" title="Consultar Guía Académica"><i class="fa fa-search"></i></a>
-					<a class="btn-gmail" href="{{url('web')}}?id={{Input::get('id')}}&id_modelo={{$modelo_guia_academica_id}}" title="Consultar Guía Académica"><i class="fa fa-search"></i></a>
-					<a class="btn-gmail" href="{{url('web')}}?id={{Input::get('id')}}&id_modelo={{$modelo_guia_academica_id}}" title="Consultar Guía Académica"><i class="fa fa-search"></i></a>
-					<a class="btn-gmail" href="{{url('web')}}?id={{Input::get('id')}}&id_modelo={{$modelo_guia_academica_id}}" title="Consultar Guía Académica"><i class="fa fa-search"></i></a>
-					<a class="btn-gmail" href="{{url('web')}}?id={{Input::get('id')}}&id_modelo={{$modelo_guia_academica_id}}" title="Consultar Guía Académica"><i class="fa fa-search"></i></a>
-					<a class="btn-gmail" href="{{url('web')}}?id={{Input::get('id')}}&id_modelo={{$modelo_guia_academica_id}}" title="Consultar Guía Académica"><i class="fa fa-search"></i></a>
-					<a class="btn-gmail" href="{{url('web')}}?id={{Input::get('id')}}&id_modelo={{$modelo_guia_academica_id}}" title="Consultar Guía Académica"><i class="fa fa-search"></i></a>
-					<a class="btn-gmail" href="{{url('web')}}?id={{Input::get('id')}}&id_modelo={{$modelo_guia_academica_id}}" title="Consultar Guía Académica"><i class="fa fa-search"></i></a>
-					<a class="btn-gmail" href="{{url('web')}}?id={{Input::get('id')}}&id_modelo={{$modelo_guia_academica_id}}" title="Consultar Guía Académica"><i class="fa fa-search"></i></a>
-					<a class="btn-gmail" href="{{url('web')}}?id={{Input::get('id')}}&id_modelo={{$modelo_guia_academica_id}}" title="Consultar Guía Académica"><i class="fa fa-search"></i></a>
-					<a class="btn-gmail" href="{{url('web')}}?id={{Input::get('id')}}&id_modelo={{$modelo_guia_academica_id}}" title="Consultar Guía Académica"><i class="fa fa-search"></i></a>
+					<!-- CALIFICACIONES -->
+					<a class="btn-gmail" onclick="calificacionesCrear()" title="Ingresar Calificaciones"><i class="fa fa-list-ol"></i></a>
+					<a class="btn-gmail" onclick="calificacionesConsultar()" title="Consultar Calificaciones"><i class="fa fa-search"></i></a>
+					@if( config('calificaciones.manejar_preinformes_academicos') == 'Si' )
+					<a class="btn-gmail" onclick="preinformeCrear()" title="Ingresar Pre-Informe"><i class="fa fa-file-pdf-o"></i></a>
+					<a class="btn-gmail" onclick="preinformeConsultar()" title="Consultar Pre-Informe"><i class="fa fa-search"></i></a>
+					@endif
+					<a class="btn-gmail" onclick="nivelacionesCrear()" title="Ingresar Nivelaciones"><i class="fa fa-check"></i></a>
+					<a class="btn-gmail" onclick="nivelacionesConsultar()" title="Consultar Nivelaciones"><i class="fa fa-search"></i></a>
+					<!-- LOGROS -->
+					<a class="btn-gmail" onclick="logrosCrear()" title="Crear Logros"><i class="fa fa-bookmark-o"></i></a>
+					<a class="btn-gmail" onclick="logrosConsultar()" title="Consultar Logros"><i class="fa fa-search"></i></a>
+					<a class="btn-gmail" onclick="logrosAdicionalesCrear()" title="Crear Logros Adicionales"><i class="fa fa-tag"></i></a>
+					<a class="btn-gmail" onclick="logrosAdicionalesConsultar()" title="Consultar Logros Adicionales"><i class="fa fa-search"></i></a>
+					<!-- PROPÓSITOS -->
+					@if( config('calificaciones.colegio_maneja_metas') == 'Si' )
+					@can('ACDO_metas_propositos')
+					<a class="btn-gmail" onclick="propositoCrear()" title="Crear Meta o Propósito"><i class="fa fa-check-square-o"></i></a>
+					<a class="btn-gmail" onclick="propositoConsultar()" title="Consultar Meta o Propósito"><i class="fa fa-search"></i></a>
+					@endcan
+					@endif
+					@can('ACDO_control_disciplinario')
+					<a class="btn-gmail" onclick="controlDisciplinarioCrear()" title="Crear Control Disciplinario"><i class="fa fa-eye"></i></a>
+					<a class="btn-gmail" onclick="controlDisciplinarioConsultar()" title="Consultar Control Disciplinario"><i class="fa fa-search"></i></a>
+					@endcan
+					<!-- LISTADO DE ESTUDIANTES Y FOROS -->
 					<a class="btn-gmail" onclick="listarEstudiantes()" title="Listado de Estudiante"><i class="fa fa-users"></i></a>
 					<a class="btn-gmail" onclick="foros()" title="Foros de Discusión"><i class="fa fa-bullhorn"></i></a>
 				</div>
@@ -45,6 +64,7 @@
 					<thead>
 						<tr>
 							<th><i class="fa fa-check-square-o"></i></th>
+							<th>#</th>
 							<th>CURSO O GRUPO</th>
 							<th>ASIGNATURA</th>
 						</tr>
@@ -56,71 +76,9 @@
 						@foreach ($listado as $fila)
 						<tr>
 							<td><input type="checkbox" value="{{$fila->curso_id.';'.$fila->id_asignatura}}" class="btn-gmail-check"></td>
+							<td>{{$contador}}</td>
 							<td>{{ $fila->Curso }}</td>
 							<td>{{ $fila->Asignatura }}</td>
-							<td>
-								<!--
-												CALIFICACIONES
-									  	-->
-								<?php
-								$modelo_preinforme_academico_id = 192;
-
-								$opcion_ingresar_calificaciones = ['link' => 'academico_docente/calificar/' . $fila->curso_id . '/' . $fila->id_asignatura . '/' . rand(0, 1000) . '?id=' . Input::get('id'), 'etiqueta' => 'Ingresar'];
-
-								$opcion_consultar_calificaciones = ['link' => 'academico_docente/revisar_calificaciones/curso_id/' . $fila->curso_id . '/' . $fila->id_asignatura . '?id=' . Input::get('id'), 'etiqueta' => 'Consultar'];
-
-
-								$opcion_ingresar_notas_nivelaciones = ['link' => 'sga_ingresar_notas_nivelaciones/' . $fila->curso_id . '/' . $fila->id_asignatura . '?id=' . Input::get('id'), 'etiqueta' => 'Ingresar nivelaciones'];
-
-								$opcion_consultar_notas_nivelaciones = ['link' => 'sga_notas_nivelaciones_revisar/' . $fila->curso_id . '/' . $fila->id_asignatura . '?id=' . Input::get('id'), 'etiqueta' => 'Consultar nivelaciones'];
-
-								?>
-								@if( config('calificaciones.manejar_preinformes_academicos') == 'Si' )
-								{{ Form::bsBtnDropdown( 'Calificaciones', 'primary', 'edit', [ 
-										  			$opcion_ingresar_calificaciones, 
-										  			$opcion_consultar_calificaciones, 
-										  			['link' => 'cali_preinforme_academico/create?id='.Input::get('id').'&id_modelo='.$modelo_preinforme_academico_id.'&curso_id='.$fila->curso_id.'&asignatura_id='.$fila->id_asignatura, 'etiqueta' => 'Pre-informe: ingresar' ], 
-										  			['link' => 'web?id='.Input::get('id').'&id_modelo='.$modelo_preinforme_academico_id.'&curso_id='.$fila->curso_id.'&asignatura_id='.$fila->id_asignatura, 'etiqueta' => 'Pre-informe: consultar' ],
-										  			[
-										  				'link' => '#', 
-										  				'etiqueta' => '--------' ],
-										  			$opcion_ingresar_notas_nivelaciones,
-										  			$opcion_consultar_notas_nivelaciones
-									  			] ) }}
-								@else
-								{{ Form::bsBtnDropdown( 'Calificaciones', 'primary', 'edit', [ 
-										  			$opcion_ingresar_calificaciones, 
-										  			$opcion_consultar_calificaciones,
-										  			[
-										  				'link' => '#', 
-										  				'etiqueta' => '--------' ],
-										  			$opcion_ingresar_notas_nivelaciones,
-										  			$opcion_consultar_notas_nivelaciones
-									  			] ) }}
-								@endif
-
-
-								{{ Form::bsBtnDropdown( 'Logros', 'success', 'tag', [ 
-									  			['link' => 'academico_docente/ingresar_logros/'.$fila->curso_id.'/'.$fila->id_asignatura.'?id='.Input::get('id').'&id_modelo='.$modelo_logros_id, 'etiqueta' => 'Ingresar'],
-									  			['link' => 'academico_docente/revisar_logros/'.$fila->curso_id.'/'.$fila->id_asignatura.'?id='.Input::get('id').'&id_modelo='.$modelo_logros_id, 'etiqueta' => 'Consultar' ],
-									  			[
-									  				'link' => '#', 
-									  				'etiqueta' => '--------' ],
-									  			['link' => 'academico_docente/ingresar_logros/'.$fila->curso_id.'/'.$fila->id_asignatura.'?id='.Input::get('id').'&id_modelo='.$modelo_logros_adicionales_id, 'etiqueta' => 'Logros adicionales'],
-									  			['link' => 'academico_docente/revisar_logros/'.$fila->curso_id.'/'.$fila->id_asignatura.'?id='.Input::get('id').'&id_modelo='.$modelo_logros_adicionales_id, 'etiqueta' => 'Consultar adicionales'] ] ) }}
-
-
-								@if( config('calificaciones.colegio_maneja_metas') == 'Si' )
-								@can('ACDO_metas_propositos')
-								{{ Form::bsBtnDropdown( 'Propósitos', 'info', 'tag', [ ['link' => 'academico_docente/ingresar_metas/'.$fila->curso_id.'/'.$fila->id_asignatura.'?id='.Input::get('id'), 'etiqueta' => 'Ingresar'], ['link' => 'academico_docente/revisar_metas/'.$fila->curso_id.'/'.$fila->id_asignatura.'?id='.Input::get('id'), 'etiqueta' => 'Consultar' ] ] ) }}
-								@endcan
-								@endif
-
-
-								@can('ACDO_control_disciplinario')
-								{{ Form::bsBtnDropdown( 'Control disciplinario', 'danger', 'eye', [ ['link' => 'matriculas/control_disciplinario/precreate/'.$fila->curso_id.'/'.$fila->id_asignatura.'?id='.Input::get('id'), 'etiqueta' => 'Ingresar'], ['link' => 'matriculas/control_disciplinario/consultar/'.$fila->curso_id.'/'.date('Y-m-d').'?id='.Input::get('id'), 'etiqueta' => 'Consultar' ] ] ) }}
-								@endcan
-							</td>
 						</tr>
 						<?php
 						$contador++;
@@ -237,6 +195,244 @@
 				//procesar uno
 				var curso_asignatura = elementos[0].split(';');
 				url = url + curso_asignatura[0] + "/" + curso_asignatura[1] + "/{{$periodo_lectivo->id}}/inicio?id={{Input::get('id')}}";
+				location.href = url;
+			} else {
+				mensaje('Alerta!', 'Solo puede procesar un curso a la vez', 'warning');
+			}
+		} else {
+			mensaje('Alerta!', 'Debe seleccionar al menos un registro', 'warning');
+		}
+	}
+
+	function calificacionesCrear() {
+		let elementos = getElementos();
+		if (elementos.length > 0) {
+			var url = "{{url('')}}/academico_docente/calificar/";
+			if (elementos.length == 1) {
+				//procesar uno
+				var curso_asignatura = elementos[0].split(';');
+				url = url + curso_asignatura[0] + "/" + curso_asignatura[1] + "/{{rand(0, 1000)}}?id={{Input::get('id')}}";
+				location.href = url;
+			} else {
+				mensaje('Alerta!', 'Solo puede procesar un curso a la vez', 'warning');
+			}
+		} else {
+			mensaje('Alerta!', 'Debe seleccionar al menos un registro', 'warning');
+		}
+	}
+
+	function calificacionesConsultar() {
+		let elementos = getElementos();
+		if (elementos.length > 0) {
+			var url = "{{url('')}}/academico_docente/revisar_calificaciones/curso_id/";
+			if (elementos.length == 1) {
+				//procesar uno
+				var curso_asignatura = elementos[0].split(';');
+				url = url + curso_asignatura[0] + "/" + curso_asignatura[1] + "?id={{Input::get('id')}}";
+				location.href = url;
+			} else {
+				mensaje('Alerta!', 'Solo puede procesar un curso a la vez', 'warning');
+			}
+		} else {
+			mensaje('Alerta!', 'Debe seleccionar al menos un registro', 'warning');
+		}
+	}
+
+	function preinformeCrear() {
+		let elementos = getElementos();
+		if (elementos.length > 0) {
+			var url = "{{url('')}}/cali_preinforme_academico/create";
+			if (elementos.length == 1) {
+				//procesar uno
+				var curso_asignatura = elementos[0].split(';');
+				url = url + "?id={{Input::get('id')}}&id_modelo={{$modelo_preinforme_academico_id}}&curso_id=" + curso_asignatura[0] + "&asignatura_id=" + curso_asignatura[1];
+				location.href = url;
+			} else {
+				mensaje('Alerta!', 'Solo puede procesar un curso a la vez', 'warning');
+			}
+		} else {
+			mensaje('Alerta!', 'Debe seleccionar al menos un registro', 'warning');
+		}
+	}
+
+	function preinformeConsultar() {
+		let elementos = getElementos();
+		if (elementos.length > 0) {
+			var url = "{{url('')}}/web";
+			if (elementos.length == 1) {
+				//procesar uno
+				var curso_asignatura = elementos[0].split(';');
+				url = url + "?id={{Input::get('id')}}&id_modelo={{$modelo_preinforme_academico_id}}&curso_id=" + curso_asignatura[0] + "&asignatura_id=" + curso_asignatura[1];
+				location.href = url;
+			} else {
+				mensaje('Alerta!', 'Solo puede procesar un curso a la vez', 'warning');
+			}
+		} else {
+			mensaje('Alerta!', 'Debe seleccionar al menos un registro', 'warning');
+		}
+	}
+
+	function nivelacionesCrear() {
+		let elementos = getElementos();
+		if (elementos.length > 0) {
+			var url = "{{url('')}}/sga_ingresar_notas_nivelaciones/";
+			if (elementos.length == 1) {
+				//procesar uno
+				var curso_asignatura = elementos[0].split(';');
+				url = url + curso_asignatura[0] + "/" + curso_asignatura[1] + "?id={{Input::get('id')}}";
+				location.href = url;
+			} else {
+				mensaje('Alerta!', 'Solo puede procesar un curso a la vez', 'warning');
+			}
+		} else {
+			mensaje('Alerta!', 'Debe seleccionar al menos un registro', 'warning');
+		}
+	}
+
+	function nivelacionesConsultar() {
+		let elementos = getElementos();
+		if (elementos.length > 0) {
+			var url = "{{url('')}}/sga_notas_nivelaciones_revisar/";
+			if (elementos.length == 1) {
+				//procesar uno
+				var curso_asignatura = elementos[0].split(';');
+				url = url + curso_asignatura[0] + "/" + curso_asignatura[1] + "?id={{Input::get('id')}}";
+				location.href = url;
+			} else {
+				mensaje('Alerta!', 'Solo puede procesar un curso a la vez', 'warning');
+			}
+		} else {
+			mensaje('Alerta!', 'Debe seleccionar al menos un registro', 'warning');
+		}
+	}
+
+	function logrosCrear() {
+		let elementos = getElementos();
+		if (elementos.length > 0) {
+			var url = "{{url('')}}/academico_docente/ingresar_logros/";
+			if (elementos.length == 1) {
+				//procesar uno
+				var curso_asignatura = elementos[0].split(';');
+				url = url + curso_asignatura[0] + "/" + curso_asignatura[1] + "?id={{Input::get('id')}}&id_modelo={{$modelo_logros_id}}";
+				location.href = url;
+			} else {
+				mensaje('Alerta!', 'Solo puede procesar un curso a la vez', 'warning');
+			}
+		} else {
+			mensaje('Alerta!', 'Debe seleccionar al menos un registro', 'warning');
+		}
+	}
+
+	function logrosConsultar() {
+		let elementos = getElementos();
+		if (elementos.length > 0) {
+			var url = "{{url('')}}/academico_docente/revisar_logros/";
+			if (elementos.length == 1) {
+				//procesar uno
+				var curso_asignatura = elementos[0].split(';');
+				url = url + curso_asignatura[0] + "/" + curso_asignatura[1] + "?id={{Input::get('id')}}&id_modelo={{$modelo_logros_id}}";
+				location.href = url;
+			} else {
+				mensaje('Alerta!', 'Solo puede procesar un curso a la vez', 'warning');
+			}
+		} else {
+			mensaje('Alerta!', 'Debe seleccionar al menos un registro', 'warning');
+		}
+	}
+
+	function logrosAdicionalesCrear() {
+		let elementos = getElementos();
+		if (elementos.length > 0) {
+			var url = "{{url('')}}/academico_docente/ingresar_logros/";
+			if (elementos.length == 1) {
+				//procesar uno
+				var curso_asignatura = elementos[0].split(';');
+				url = url + curso_asignatura[0] + "/" + curso_asignatura[1] + "?id={{Input::get('id')}}&id_modelo={{$modelo_logros_adicionales_id}}";
+				location.href = url;
+			} else {
+				mensaje('Alerta!', 'Solo puede procesar un curso a la vez', 'warning');
+			}
+		} else {
+			mensaje('Alerta!', 'Debe seleccionar al menos un registro', 'warning');
+		}
+	}
+
+	function logrosAdicionalesConsultar() {
+		let elementos = getElementos();
+		if (elementos.length > 0) {
+			var url = "{{url('')}}/academico_docente/revisar_logros/";
+			if (elementos.length == 1) {
+				//procesar uno
+				var curso_asignatura = elementos[0].split(';');
+				url = url + curso_asignatura[0] + "/" + curso_asignatura[1] + "?id={{Input::get('id')}}&id_modelo={{$modelo_logros_adicionales_id}}";
+				location.href = url;
+			} else {
+				mensaje('Alerta!', 'Solo puede procesar un curso a la vez', 'warning');
+			}
+		} else {
+			mensaje('Alerta!', 'Debe seleccionar al menos un registro', 'warning');
+		}
+	}
+
+	function propositoCrear() {
+		let elementos = getElementos();
+		if (elementos.length > 0) {
+			var url = "{{url('')}}/academico_docente/ingresar_metas/";
+			if (elementos.length == 1) {
+				//procesar uno
+				var curso_asignatura = elementos[0].split(';');
+				url = url + curso_asignatura[0] + "/" + curso_asignatura[1] + "?id={{Input::get('id')}}";
+				location.href = url;
+			} else {
+				mensaje('Alerta!', 'Solo puede procesar un curso a la vez', 'warning');
+			}
+		} else {
+			mensaje('Alerta!', 'Debe seleccionar al menos un registro', 'warning');
+		}
+	}
+
+	function propositoConsultar() {
+		let elementos = getElementos();
+		if (elementos.length > 0) {
+			var url = "{{url('')}}/academico_docente/revisar_metas/";
+			if (elementos.length == 1) {
+				//procesar uno
+				var curso_asignatura = elementos[0].split(';');
+				url = url + curso_asignatura[0] + "/" + curso_asignatura[1] + "?id={{Input::get('id')}}";
+				location.href = url;
+			} else {
+				mensaje('Alerta!', 'Solo puede procesar un curso a la vez', 'warning');
+			}
+		} else {
+			mensaje('Alerta!', 'Debe seleccionar al menos un registro', 'warning');
+		}
+	}
+
+	function controlDisciplinarioCrear() {
+		let elementos = getElementos();
+		if (elementos.length > 0) {
+			var url = "{{url('')}}/matriculas/control_disciplinario/precreate/";
+			if (elementos.length == 1) {
+				//procesar uno
+				var curso_asignatura = elementos[0].split(';');
+				url = url + curso_asignatura[0] + "/" + curso_asignatura[1] + "?id={{Input::get('id')}}";
+				location.href = url;
+			} else {
+				mensaje('Alerta!', 'Solo puede procesar un curso a la vez', 'warning');
+			}
+		} else {
+			mensaje('Alerta!', 'Debe seleccionar al menos un registro', 'warning');
+		}
+	}
+
+	function controlDisciplinarioConsultar() {
+		let elementos = getElementos();
+		if (elementos.length > 0) {
+			var url = "{{url('')}}/matriculas/control_disciplinario/consultar/";
+			if (elementos.length == 1) {
+				//procesar uno
+				var curso_asignatura = elementos[0].split(';');
+				url = url + curso_asignatura[0] + "/{{date('Y-m-d')}}?id={{Input::get('id')}}";
 				location.href = url;
 			} else {
 				mensaje('Alerta!', 'Solo puede procesar un curso a la vez', 'warning');
