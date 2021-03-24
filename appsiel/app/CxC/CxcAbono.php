ee1e5e2b879950825487efa0dea92f94ce72a2f3
Adalberto Perez <adalberto.perez@appsiel.com.co>
2021-03-24 10:45:03 -0500

    arreglo index cxcabonos y cxpabonos

--- appsiel/app/CxC/CxcAbono.php
+++ appsiel/app/CxC/CxcAbono.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 use DB;
 use Auth;
 
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
+
 class CxcAbono extends Model
 {
     // Tabla auxiliar para llevar el registro de los abonos a los documentos de cxc.
@@ -19,23 +22,82 @@ class CxcAbono extends Model
 
     public static function consultar_registros( $nro_registros, $search )
     {
-        return CxcAbono::leftJoin('core_tipos_docs_apps', 'core_tipos_docs_apps.id', '=', 'cxc_abonos.core_tipo_doc_app_id')
-            ->leftJoin('core_tipos_docs_apps AS tipo_docs_cxc', 'tipo_docs_cxc.id', '=', 'cxc_abonos.doc_cxc_tipo_doc_id')
-            ->leftJoin('core_terceros', 'core_terceros.id', '=', 'cxc_abonos.core_tercero_id')
-            ->where('cxc_abonos.core_empresa_id', Auth::user()->empresa_id)
-            ->select(
-                'cxc_abonos.fecha AS campo1',
-                DB::raw('CONCAT(core_tipos_docs_apps.prefijo," ",cxc_abonos.consecutivo) AS campo2'),
-                DB::raw('CONCAT(core_terceros.nombre1," ",core_terceros.otros_nombres," ",core_terceros.apellido1," ",core_terceros.apellido2," ",core_terceros.razon_social) AS campo3'),
-                DB::raw('CONCAT(tipo_docs_cxc.prefijo," ",cxc_abonos.doc_cxc_consecutivo) AS campo4'),
-                'cxc_abonos.abono AS campo5',
-                'cxc_abonos.id AS campo6'
-            )
-            ->orWhere(DB::raw('CONCAT(core_tipos_docs_apps.prefijo," ",cxc_abonos.consecutivo)'), "LIKE", "%$search%")
-            ->orWhere("cxc_abonos.fecha", "LIKE", "%$search%")
-            ->orWhere("core_terceros.descripcion", "LIKE", "%$search%")
-            ->orderBy('cxc_abonos.created_at', 'DESC')
-            ->paginate($nro_registros);
+        $collection = CxcAbono::leftJoin('core_tipos_docs_apps', 'core_tipos_docs_apps.id', '=', 'cxc_abonos.core_tipo_doc_app_id')
+                        ->leftJoin('core_tipos_docs_apps AS tipo_docs_cxc', 'tipo_docs_cxc.id', '=', 'cxc_abonos.doc_cxc_tipo_doc_id')
+                        ->leftJoin('core_terceros', 'core_terceros.id', '=', 'cxc_abonos.core_tercero_id')
+                        ->where('cxc_abonos.core_empresa_id', Auth::user()->empresa_id)
+                        ->select(
+                            'cxc_abonos.fecha AS campo1',
+                            DB::raw('CONCAT(core_tipos_docs_apps.prefijo," ",cxc_abonos.consecutivo) AS campo2'),
+                            DB::raw('CONCAT(core_terceros.nombre1," ",core_terceros.otros_nombres," ",core_terceros.apellido1," ",core_terceros.apellido2," ",core_terceros.razon_social) AS campo3'),
+                            DB::raw('CONCAT(tipo_docs_cxc.prefijo," ",cxc_abonos.doc_cxc_consecutivo) AS campo4'),
+                            'cxc_abonos.abono AS campo5',
+                            'cxc_abonos.id AS campo6'
+                        )
+                        ->orderBy('cxc_abonos.created_at', 'DESC')
+                        ->get();
+
+        //hacemos el filtro de $search si $search tiene contenido
+        $nuevaColeccion = [];
+        if (count($collection) > 0) {
+            if (strlen($search) > 0) {
+                $nuevaColeccion = $collection->filter(function ($c) use ($search) {
+                    if ( self::likePhp([$c->campo1, $c->campo2, $c->campo3, $c->campo4, $c->campo5, $c->campo6, $c->campo7, $c->campo8], $search)) {
+                        return $c;
+                    }
+                });
+            } else {
+                $nuevaColeccion = $collection;
+            }
+        }
+
+        $request = request(); //obtenemos el Request para obtener la url y la query builder
+
+        if ( empty($nuevaColeccion) )
+        {
+            return $array = new LengthAwarePaginator([], 1, 1, 1, [
+                                                                    'path' => $request->url(),
+                                                                    'query' => $request->query(),
+                                                                ]);
+        }
+        
+        //obtenemos el numero de la página actual, por defecto 1
+        $page = 1;
+        if (isset($_GET['page'])) {
+            $page = $_GET['page'];
+        }
+        $total = count($nuevaColeccion); //Total para contar los registros mostrados
+        $starting_point = ($page * $nro_registros) - $nro_registros; // punto de inicio para mostrar registros
+        $array = $nuevaColeccion->slice($starting_point, $nro_registros); //indicamos desde donde y cuantos registros mostrar
+        $array = new LengthAwarePaginator($array, $total, $nro_registros, $page, [
+            'path' => $request->url(),
+            'query' => $request->query(),
+        ]); //finalmente se pagina y organiza la coleccion a devolver con todos los datos
+
+        return $array;
+    }
+
+    /**
+     * SQL Like operator in PHP.
+     * Returns TRUE if match else FALSE.
+     * @param array $valores_campos_seleccionados de campos donde se busca
+     * @param string $searchTerm termino de busqueda
+     * @return bool
+     */
+    public static function likePhp($valores_campos_seleccionados, $searchTerm)
+    {
+        $encontrado = false;
+        $searchTerm = str_slug($searchTerm); // Para eliminar acentos
+        foreach ($valores_campos_seleccionados as $valor_campo)
+        {
+            $str = str_slug($valor_campo);
+            $pos = strpos($str, $searchTerm);
+            if ($pos !== false)
+            {
+                $encontrado = true;
+            }
+        }
+        return $encontrado;
     }
 
     public static function sqlString($search)
