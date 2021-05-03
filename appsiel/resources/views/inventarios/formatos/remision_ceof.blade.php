2e54e8e085cc1d5597e10565fbb1d76de0d42c55
Ricardo Garces <ricardo.garces@appsiel.com.co>
2021-05-03 18:37:01 -0500

    remision

--- appsiel/resources/views/inventarios/formatos/remision_ceof.blade.php
+++ appsiel/resources/views/inventarios/formatos/remision_ceof.blade.php
@@ -36,7 +36,9 @@
     </div>
 
     <div style="position: absolute; left: 144mm; top: 78.5mm;">
+        @if ($doc_encabezado->contacto_cliente->tercero != null)
         {{ $doc_encabezado->contacto_cliente->tercero->descripcion }}
+        @endif
     </div>
 
     <div style="position: absolute; left: 30mm; top: 89.5mm;">
