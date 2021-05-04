4afc579660d2eac9581ffda3ec7193d123460452
Ricardo Garces <ricardo.garces@appsiel.com.co>
2021-05-04 07:41:24 -0500

    correciones cotizacion

--- appsiel/resources/views/ventas/cotizaciones/documento_imprimir2.blade.php
+++ appsiel/resources/views/ventas/cotizaciones/documento_imprimir2.blade.php
@@ -58,8 +58,9 @@ use App\Core\Tercero;
     .text-left{
         text-align: left !important;
     }
-    .text-indent{
-        padding-left: 20px;
+    .text-indent, .text-indent > *{
+        padding-left: 10px !important;
+        text-align: justify !important;
     }
     .contenido th{
         color: black !important;
@@ -109,7 +110,7 @@ use App\Core\Tercero;
            
 			?>	
             </td>
-        <td>Telefono: {{ $empresa->telefono1 }}</td>
+        <td>Teléfono: {{ $empresa->telefono1 }}</td>
     </tr>
     <tr>
         <td>Email: <a href="mailto:{{ $empresa->email }}">{{ $empresa->email }}</a></td>
@@ -153,12 +154,17 @@ use App\Core\Tercero;
             <b>Fecha:</b>
         </td>
         <td>
-            {{ $doc_encabezado->fecha }}
+            <?php
+                $fecha = date_create($doc_encabezado->fecha);
+                $meses = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");                       
+                $fecha_final = date_format($fecha,"d")." ".$meses[date_format($fecha,"n")-1]." ".date_format($fecha,"Y");
+            ?>
+            {{ $fecha_final }}
         </td>
     </tr>
     <tr>
         <td>
-            <b>Telefono: </b>
+            <b>Teléfono: </b>
         </td>
         <td colspan="3">
             {{ $contacto->telefono1 }}
@@ -175,13 +181,18 @@ use App\Core\Tercero;
             <b>Valido hasta:</b>
         </td>
         <td>
-            {{ $doc_encabezado->fecha_vencimiento }}
+            <?php
+                $fecha = date_create($doc_encabezado->fecha_vencimiento);
+                $meses = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");                       
+                $fecha_final = date_format($fecha,"d")." ".$meses[date_format($fecha,"n")-1]." ".date_format($fecha,"Y");
+            ?>
+            {{ $fecha_final }}
         </td>
     </tr>
 </table>
 <hr>
     <br>
-    <b>Oferta Tecnica: &nbsp;&nbsp;</b> 
+    <b>Oferta Técnica: &nbsp;&nbsp;</b> 
     <br>
     <p class="info text-indent">
         <?php echo $doc_encabezado->descripcion ?>
@@ -189,7 +200,7 @@ use App\Core\Tercero;
     
 <br>
 <hr>
-<b>Oferta Economica: &nbsp;&nbsp;</b> 
+<b>Oferta Económica: &nbsp;&nbsp;</b> 
 <table class="contenido">
     {{ Form::bsTableHeader(['Item','Producto','Cant.','Vr. unit.','IVA','DTO','Total']) }}
     <tbody>
@@ -217,12 +228,11 @@ use App\Core\Tercero;
                 ?>
             <td class="text-left" width="250px"> {{ $descripcion_item }} </td>
             <td class="text-center"> {{ number_format( $linea->cantidad, 0, ',', '.') }} </td>
-            <td> {{ '$  '.number_format( $linea->precio_unitario / (1+$linea->tasa_impuesto/100) , 0, ',', '.') }}
+            <td> {{ '$  '.number_format( $linea->precio_unitario / (1+$linea->tasa_impuesto/100) , 2, ',', '.') }}
             </td>
             <td class="text-center"> {{ number_format( $linea->tasa_impuesto, 0, ',', '.').'%' }} </td>
-            <td> {{ '$  '.number_format( $linea->precio_unitario / (1+$linea->tasa_impuesto/100) * $linea->cantidad, 0, ',', '.') }}
-            </td>
-            <td> {{ '$  '.number_format( $linea->precio_total, 0, ',', '.') }} </td>
+            <td style="text-align: right;"> $ &nbsp;{{ number_format( $linea->valor_total_descuento, 2, ',', '.') }} </td>
+            <td> {{ '$  '.number_format( $linea->precio_total, 2, ',', '.') }} </td>
         </tr>
         <?php
                 $i++;
@@ -282,7 +292,7 @@ use App\Core\Tercero;
     </tr>
     <tr>
         <td width="30%"><b>Condición de Venta</b></td>
-        <td>{{ $doc_encabezado->condicion_pago }}</td>
+        <td style="text-transform: capitalize">{{ $doc_encabezado->condicion_pago }}</td>
     </tr>
     <tr>
         <td width="30%"><b>Plazo de entrega</b></td>
@@ -290,8 +300,8 @@ use App\Core\Tercero;
     </tr>
 </table>
 <hr>
-<b>Condiciones Comerciales</b>
-<p class="text-indent">{{ $otroscampos->terminos_y_condiciones }}</p>
+<h1>Condiciones:</h1>
+<div class="text-indent"><?php echo $otroscampos->terminos_y_condiciones ?></div>
 <br>
 <div class="encabezado">
     <p style="text-align: right; font-size: 18px; padding-right: 30px">{{ $empresa->descripcion }}</p>
@@ -299,3 +309,22 @@ use App\Core\Tercero;
 <div class="page-break"></div>
 </body>
 </html>
+
+<p><strong><u>CONDICIONES COMERCIALES:</u></strong></p>
+<p>La aceptaci&oacute;n del presente presupuesto puede realizarse v&iacute;a email para coordinar los trabajos a ejecutar.</p>
+
+<p><strong><u>ALCANCE DE LA PROPUESTA:</u></strong></p>
+
+<ol>
+	<li>El presente presupuesto es v&aacute;lido para horas normales y en horario extendido de 17 a 21 hs para d&iacute;as de semana en caso que el cliente propusiese comienzo en d&iacute;a distinto, o que por motivos ajenos a Seizo los trabajos o traslado se tuvieran que efectuar en horarios o d&iacute;as especiales; eso podr&aacute; provocar cambios en el presente presupuesto y Seizo se reserva el derecho de exigir adicionales.</li>
+	<li>En caso que fuera necesario aplicar alg&uacute;n repuesto no incluido en el presupuesto y el cliente no cuente con los mismos, esto ser&aacute; cotizado como adicional.</li>
+	<li>Limpieza general de los equipos no est&aacute; incluida.</li>
+	<li>Si por indisponibilidad del equipo por parte del comitente se provocaran tiempos de espera que atenten contra la realizaci&oacute;n de los trabajos previstos dentro del plazo establecido, esto ser&aacute; notificado al cliente y se acordar&aacute; con &eacute;l en funci&oacute;n del nivel de avance a alcanzar:
+<ul>
+	<li>La terminaci&oacute;n de los trabajos posibles dentro del plazo previsto, y el acuerdo de una nueva visita.</li>
+	<li>La prolongaci&oacute;n del cronograma hasta alcanzar la culminaci&oacute;n de los trabajos, si otros compromisos para los t&eacute;cnicos Seizo no lo impidieran y esto fuese posible para el cliente. En cualquiera de los casos, el cargo por d&iacute;as adicionales y dem&aacute;s gastos asociados (alojamientos, comidas, etc.) ser&aacute;n tomados como adicional y cotizados aparte.</li>
+</ul>
+    <li>Si por motivos de fuerza mayor (medidas de fuerza, problemas energ&eacute;ticos, causas naturales, etc.) no se pudieran iniciar o continuar los trabajos, esto ser&aacute; notificado al cliente y se acordar&aacute; con &eacute;l de igual forma que el punto anterior.</li>
+    <li>El presente presupuesto contempla documentaci&oacute;n de ingreso est&aacute;ndar (ART, Seguro de Vida, EPP, estudios m&eacute;dicos anuales regulares).</li>
+</li>
+</ol>
