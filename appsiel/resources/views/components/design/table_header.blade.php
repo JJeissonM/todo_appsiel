8675cbc0cdbf1ec8383269be910f868c8bb8cebd
Adalberto Perez <adalberto.perez@appsiel.com.co>
2021-05-06 21:14:59 -0500

    arreglo vista index

--- appsiel/resources/views/components/design/table_header.blade.php
+++ appsiel/resources/views/components/design/table_header.blade.php
@@ -1,8 +1,9 @@
 <thead>
 	<tr style="background-color: #50B794; vertical-align: middle !important;">
 		<?php
-			for($li=0;$li<count($headers);$li++){
-				echo '<th  style="text-align: center; vertical-align: middle !important; font-weight: bolder; color: #000; text-transform: uppercase;" title="' . $headers[$li] . '">'.$headers[$li].'</th>';
+			for($li=0;$li<count($headers);$li++)
+			{
+				echo '<th style="text-align: center; vertical-align: middle !important; font-weight: bolder; color: #000; text-transform: uppercase;">'.$headers[$li].'</th>';
 			}
 		?>
 	</tr>
