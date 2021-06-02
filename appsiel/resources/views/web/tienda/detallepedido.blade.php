8290a55b2ab5891ce2cdbc3979064f33caa833ae
Ricardo Garces <ricardo.garces@appsiel.com.co>
2021-06-01 20:32:23 -0500

    totalis

--- appsiel/resources/views/web/tienda/detallepedido.blade.php
+++ appsiel/resources/views/web/tienda/detallepedido.blade.php
@@ -222,7 +222,7 @@
                                                 data-render="button"
                                                 data-public-key="pub_test_enceqQgcwYlN9PQozadt9XBRa9VLCnsf"
                                                 data-currency="COP"
-                                                data-amount-in-cents="{{ number_format(($total_pagar+5000).'00',0,'','') }}"
+                                                data-amount-in-cents="{{ number_format(($total_pagar+5000),2,'','') }}"
                                                 data-reference="{{ $doc_encabezado->id }}"
                                                 data-redirect-url="{{ url('ecommerce/public/detallepedido/').'/'.$doc_encabezado->id }}"
                                                 >
@@ -271,7 +271,7 @@
                                             data-render="button"
                                             data-public-key="pub_test_enceqQgcwYlN9PQozadt9XBRa9VLCnsf"
                                             data-currency="COP"
-                                            data-amount-in-cents="{{ number_format($total_pagar.'00',0,'','') }}"
+                                            data-amount-in-cents="{{ number_format($total_pagar,2,'','') }}"
                                             data-reference="{{ $doc_encabezado->id }}"
                                             data-redirect-url="{{ url('ecommerce/public/detallepedido/').'/'.$doc_encabezado->id }}"
                                             >
