f0759964c6fee68ea023b85367ec758242c7e6ed
Adalberto Perez <ing.adalbertoperez@gmail.com>
2021-08-02 13:46:59 -0500

    Fact. Elect. Almacenar Campos Resultado Envio

--- appsiel/app/FacturacionElectronica/DATAICO/FacturaGeneral.php
+++ appsiel/app/FacturacionElectronica/DATAICO/FacturaGeneral.php
@@ -45,13 +45,14 @@ class FacturaGeneral
 
    public function procesar_envio_factura()
    {
-      /*$json_doc_electronico_enviado = $this->preparar_cadena_json();
-      
+      $json_doc_electronico_enviado = $this->preparar_cadena_json();
+      /*
 dd($json_doc_electronico_enviado);
-      */$consecutivo = 108;
+      
+      $consecutivo = 105;
       $json_doc_electronico_enviado = '{"actions": {"send_dian": true,"send_email": false,"email": "ing.adalbertoperez@gmail.com"},"invoice": {"env": "PRUEBAS","dataico_account_id": "a2532b03-a8bf-4514-a4e8-5fd7ec0499e9","number":'.$consecutivo.',"issue_date": "2021-07-31","payment_date": "2021-07-31","invoice_type_code": "FACTURA_VENTA","payment_means_type": "CREDITO","payment_means": "MUTUAL_AGREEMENT","numbering":{"resolution_number":"18760000001","prefix":"SETT","flexible":true}, "customer": {"email": "ing.adalbertoperez@gmail.com","phone": "06 50 27 98 72","party_type": "PERSONA_NATURAL","company_name": "HODGES DILLON HADASSAH","first_name":"HADASSAH","family_name":"HODGES","party_identification": "163506063","tax_level_code": "SIMPLIFICADO","regimen": "ORDINARIO","department": "CESAR","city": "VALLEDUPAR","address_line": "Apartado núm.: 297, 6199 Ullamcorper Ctra."},"items": [{"sku": "6","description": "ARROZ DIANA 3 KL","quantity": 2,"price": 7500,"taxes": [  {    "tax_rate": 0,"tax_category": "IVA"}]},{"sku": "8","description": "LANGOSTINO","quantity": 1.5,"price": 46500,"discount_rate": 7,"taxes": [  {    "tax_rate": 0,"tax_category": "IVA"}]},{"sku": "43","description": "LANGOSTA GRANDE","quantity": 3.23,"price": 40840.3361,"discount_rate": 10,"taxes": [  {    "tax_rate": 19,"tax_category": "IVA"}]}],"charges": []}}';
-
-      dd(json_decode( $json_doc_electronico_enviado )->invoice);
+*/
+      //dd(json_decode( $json_doc_electronico_enviado )->invoice);
       try {
           $client = new Client(['base_uri' => $this->url_emision]);
 
@@ -71,15 +72,14 @@ dd($json_doc_electronico_enviado);
       $array_respuesta = json_decode( (string) $response->getBody(), true );
       $array_respuesta['codigo'] = $response->getStatusCode();
 
-      dd( $array_respuesta, $response->getStatusCode() );
+      //dd( $array_respuesta, $response->getStatusCode() );
 
       switch ( $response->getStatusCode() ) {
          case '201':
             /*La solicitud se ha cumplido y ha dado lugar a la creación de un nuevo recurso, la factura fue creada satisfactoriamente.*/
             
             // Almacenar resultado en base de datos para Auditoria
-            $obj_resultado = new ResultadoEnvio;
-            $mensaje = $obj_resultado->almacenar_resultado( $array_respuesta, json_decode( $json_doc_electronico_enviado ), $this->doc_encabezado->id );
+            
 
             break;
          
@@ -100,9 +100,10 @@ dd($json_doc_electronico_enviado);
             break;
       }
 
-      
+      $obj_resultado = new ResultadoEnvio;
+      $mensaje = $obj_resultado->almacenar_resultado( $array_respuesta, json_decode( $json_doc_electronico_enviado ), $this->doc_encabezado->id );
 
-      dd( $json );
+      return $mensaje;
    }
 
    public function preparar_cadena_json()
@@ -167,7 +168,14 @@ dd($json_doc_electronico_enviado);
             $string_items .= ',';
          }
 
-         $string_items .= '{"sku": "' . $linea->item->id . '","description": "' . $linea->item->descripcion . '","quantity": ' . abs( number_format( $linea->cantidad, $this->cantidadDecimales, '.', '') ) . ',"price": ' . abs( number_format($linea->base_impuesto, $this->cantidadDecimales, '.', '') ) . ',"discount_rate": ' . $linea->tasa_descuento . ',"taxes": [  {    "tax_rate": ' . $linea->tasa_impuesto . ',"tax_category": "IVA"}]}';
+         $string_items .= '{"sku": "' . $linea->item->id . '","description": "' . $linea->item->descripcion . '","quantity": ' . abs( number_format( $linea->cantidad, $this->cantidadDecimales, '.', '') ) . ',"price": ' . abs( number_format($linea->base_impuesto, $this->cantidadDecimales, '.', '') );
+
+         if ( $linea->tasa_descuento != 0 )
+         {
+            $string_items .= ',"discount_rate": ' . $linea->tasa_descuento;
+         } 
+
+         $string_items .= ',"taxes": [  {    "tax_rate": ' . $linea->tasa_impuesto . ',"tax_category": "IVA"}]}';
          $es_primera_linea = false;
       }
 
--- appsiel/app/FacturacionElectronica/DATAICO/ResultadoEnvio.php
+++ appsiel/app/FacturacionElectronica/DATAICO/ResultadoEnvio.php
@@ -8,9 +8,21 @@ class ResultadoEnvio
 {
 	public function almacenar_resultado( $resultado_original, $obj_documento_enviado, $encabezado_factura_id )
 	{
-
         $resultado_almacenar = $this->formatear_resultado( $resultado_original, $obj_documento_enviado, $encabezado_factura_id );
         ResultadoEnvioDocumento::create( $resultado_almacenar );
+        $resultado_original['reglasNotificacionDIAN'] = $resultado_almacenar['reglasNotificacionDIAN'];
+        $resultado_original['fechaRespuesta'] = $resultado_almacenar['fechaRespuesta'];
+
+        $resultado_original['cufe'] = $resultado_almacenar['cufe'];
+    	$resultado_original['consecutivoDocumento'] = $resultado_almacenar['consecutivoDocumento'];
+
+    	$resultado_original['esValidoDian'] = $resultado_almacenar['esValidoDian'];
+    	$resultado_original['fechaAceptacionDIAN'] = $resultado_almacenar['fechaAceptacionDIAN'];
+
+    	$resultado_original['hash'] = $resultado_almacenar['hash'];
+    	$resultado_original['qr'] = $resultado_almacenar['qr'];
+    	$resultado_original['xml'] = $resultado_almacenar['xml'];
+    	$resultado_original['resultado'] = $resultado_almacenar['resultado'];
 
         return $this->get_mensaje( $resultado_original );
 	}
@@ -18,18 +30,32 @@ class ResultadoEnvio
 
     public function formatear_resultado( $resultado, $obj_documento_enviado, $encabezado_factura_id )
     {
-    	/*
-			CAMPOS DEL RESULTADO A ALMACENAR
-    	'', '', '', '', '', '', '', '', 'mensaje', 'mensajesValidacion', '', '', '', '', '', '', '', '', '', ''
-    	*/
-
         $resultado_almacenar['vtas_doc_encabezado_id'] = $encabezado_factura_id;
         $resultado_almacenar['nombre'] = json_encode( $obj_documento_enviado ); // CONVERTIR A STRING
 
-        if ( $resultado['codigo'] == 201 )
+        $resultado_almacenar["fechaRespuesta"] = '';
+        $resultado_almacenar["reglasNotificacionDIAN"] = '';
+
+    	$resultado_almacenar['cufe'] = '';
+    	$resultado_almacenar['consecutivoDocumento'] = '';
+
+    	$resultado_almacenar['esValidoDian'] = '';
+    	$resultado_almacenar['fechaAceptacionDIAN'] = '';
+    	$resultado_almacenar['esValidoDian'] = 0;
+    	$resultado_almacenar['fechaAceptacionDIAN'] = '';
+
+    	$resultado_almacenar['hash'] = '';
+    	$resultado_almacenar['qr'] = '';
+    	$resultado_almacenar['xml'] = '';
+    	$resultado_almacenar['resultado'] = 'Error';
+
+        /* 201: La solicitud se ha cumplido y ha dado lugar a la creación de un nuevo recurso, la factura fue creada satisfactoriamente.*/
+        if ( $resultado['codigo'] == 201 ) // 
         {
         	$resultado_almacenar['cufe'] = $resultado['cufe'];
         	$resultado_almacenar['consecutivoDocumento'] = $resultado['number'];
+
+        	$resultado_almacenar['fechaRespuesta'] = $resultado['issue_date'];
 
         	$resultado_almacenar['esValidoDian'] = 1;
         	$resultado_almacenar['fechaAceptacionDIAN'] = $resultado['issue_date'];
@@ -46,55 +72,19 @@ class ResultadoEnvio
 
         }
 
-    	$mensaje = '';
-    	if ( !is_null( $resultado['mensajesValidacion'] ) )
-		{
-			if ( gettype( $resultado["mensajesValidacion"]->string ) == 'string' )
-			{
-				$mensaje .= $resultado["mensajesValidacion"]->string . '\n';
-			}else{
-				foreach ($resultado["mensajesValidacion"]->string as $key => $value) 
-				{
-					$mensaje .= $value . '\n';
-				}
-			}				
-		}
-		$resultado["mensajesValidacion"] = $mensaje;
-
-		
-    	$mensaje = '';
-    	if ( !is_null( $resultado['reglasNotificacionDIAN'] ) )
-		{
+        if ( $resultado['codigo'] == 500 ) // 
+        {
 			$mensaje = '<br>Notificaciones DIAN<br>';
-			if ( gettype( $resultado["reglasNotificacionDIAN"]->string ) == 'string' )
-			{
-				$mensaje .= $resultado["reglasNotificacionDIAN"]->string . '\n';
-			}else{
-				foreach ($resultado["reglasNotificacionDIAN"]->string as $key => $value) 
-				{
-					$mensaje .= $value . '\n';
-				}
-			}
-		}
-		$resultado["reglasNotificacionDIAN"] = $mensaje;
-		
-    	$mensaje = '';
-    	if ( !is_null( $resultado['reglasValidacionDIAN'] ) )
-		{
-			$mensaje = '<br>Validaciones DIAN<br>';
-			if ( gettype( $resultado["reglasValidacionDIAN"]->string ) == 'string' )
-			{
-				$mensaje .= $resultado["reglasValidacionDIAN"]->string . '\n';
-			}else{
-				foreach ($resultado["reglasValidacionDIAN"]->string as $key => $value) 
-				{
-					$mensaje .= $value . '\n';
-				}
-			}
-		}
-		$resultado["reglasValidacionDIAN"] = $mensaje;
+			$errores = $resultado['errors'];
+        	foreach ( $errores as $linea_resultado )
+        	{
+        		//dd($linea_resultado);
+				$mensaje .= $linea_resultado['error'] . '\n';
+        	}
+        	$resultado_almacenar["reglasNotificacionDIAN"] = $mensaje;
+        }
 
-		return $resultado;
+		return $resultado_almacenar;
     }
 
 
@@ -104,81 +94,19 @@ class ResultadoEnvio
     	$mensaje = (object)['tipo'=>'','contenido'=>''];
 
     	switch ( $resultado["codigo"] ) {
-    		case '200':
-    			$mensaje->tipo = 'flash_message';
-    			$mensaje->contenido = '<h3>Documento enviado correctamente hacia el proveedor tecnológico</h3>';
-    			$mensaje->contenido .= "Código: " .$resultado["codigo"] ."</br>Mensaje:  " .$resultado["mensaje"] ."</br>Consecutivo:  " .$resultado["consecutivoDocumento"] ."</br>CUFE:  " .$resultado["cufe"] ."</br>Fecha de Respuesta:  " .$resultado["fechaRespuesta"] ."</br>Hash:  " .$resultado["hash"] ."</br>Reglas de validación DIAN:  " .$resultado["reglasValidacionDIAN"] ."</br>Resultado:  " .$resultado["resultado"] ."</br>Tipo de CUFE:  " .$resultado["tipoCufe"] ."</br>Mensaje Validación:  ";
-
-    			if ( !is_null( $resultado['mensajesValidacion'] ) )
-				{
-					if ( gettype( $resultado["mensajesValidacion"]->string ) == 'string' )
-					{
-						$mensaje->contenido .= "</br>" .$resultado["mensajesValidacion"]->string;
-					}else{
-						foreach ($resultado["mensajesValidacion"]->string as $key => $value) 
-						{
-							$mensaje->contenido .= "</br>" . $value;
-						}
-					}				
-				}
-    			break;
-
     		case '201':
     			$mensaje->tipo = 'flash_message';
-    			$mensaje->contenido = '<h3>Documento enviado correctamente hacia la DIAN</h3>';
-    			$mensaje->contenido .= "Código: " .$resultado["codigo"] ."</br>Mensaje:  " .$resultado["mensaje"] ."</br>Consecutivo:  " .$resultado["consecutivoDocumento"] ."</br>CUFE:  " .$resultado["cufe"] ."</br>Fecha de Respuesta:  " .$resultado["fechaRespuesta"] ."</br>Hash:  " .$resultado["hash"] ."</br>Reglas de validación DIAN:  " .$resultado["reglasValidacionDIAN"] ."</br>Resultado:  " .$resultado["resultado"] ."</br>Tipo de CUFE:  " .$resultado["tipoCufe"] ."</br>Mensaje Validación:  ";
-
-    			if ( !is_null( $resultado['mensajesValidacion'] ) )
-				{
-					if ( gettype( $resultado["mensajesValidacion"]->string ) == 'string' )
-					{
-						$mensaje->contenido .= "</br>" .$resultado["mensajesValidacion"]->string;
-					}else{
-						foreach ($resultado["mensajesValidacion"]->string as $key => $value) 
-						{
-							$mensaje->contenido .= "</br>" . $value;
-						}
-					}				
-				}
-    			break;
-
-    		case '208':
-    			$mensaje->tipo = 'flash_message';
-    			$mensaje->contenido = '<h3>Documento enviado correctamente hacia la DIAN</h3>';
-    			$mensaje->contenido .= "Código: " .$resultado["codigo"] ."</br>Mensaje:  " .$resultado["mensaje"] ."</br>Consecutivo:  " .$resultado["consecutivoDocumento"] ."</br>CUFE:  " .$resultado["cufe"] ."</br>Fecha de Respuesta:  " .$resultado["fechaRespuesta"] ."</br>Hash:  " .$resultado["hash"] ."</br>Reglas de validación DIAN:  " .$resultado["reglasValidacionDIAN"] ."</br>Resultado:  " .$resultado["resultado"] ."</br>Tipo de CUFE:  " .$resultado["tipoCufe"] ."</br>Mensaje Validación:  ";
-
-    			if ( !is_null( $resultado['mensajesValidacion'] ) )
-				{
-					if ( gettype( $resultado["mensajesValidacion"]->string ) == 'string' )
-					{
-						$mensaje->contenido .= "</br>" .$resultado["mensajesValidacion"]->string;
-					}else{
-						foreach ($resultado["mensajesValidacion"]->string as $key => $value) 
-						{
-							$mensaje->contenido .= "</br>" . $value;
-						}
-					}				
-				}
+    			$mensaje->contenido = '<h3>Documento enviado correctamente hacia el proveedor tecnológico</h3>';
+    			$mensaje->contenido .= "Código: " .$resultado["codigo"] ."</br>Consecutivo:  " .$resultado["consecutivoDocumento"] ."</br>CUFE:  " .$resultado["cufe"] ."</br>Fecha de Respuesta:  " .$resultado["fechaRespuesta"] ."</br>Hash:  " .$resultado["hash"] ."</br>Reglas de validación DIAN:  " .$resultado["reglasNotificacionDIAN"] ."</br>Resultado:  " .$resultado["resultado"];
     			break;
     		
     		default:
     			$mensaje->tipo = 'mensaje_error';
 
-    			$mensaje->contenido = '<h3>Documento enviado hacia el proveedor tecnológico. Presenta errores de validación.</h3>';
-				$mensaje->contenido .= "Código: " .$resultado["codigo"] ."</br>Mensaje:  " .$resultado["mensaje"] ."</br>Fecha de Respuesta:  " .$resultado["fechaRespuesta"] ."</br>Mensaje Validación:  ";
+    			$mensaje->contenido = '<h3>Documento NO fue enviado hacia el proveedor tecnológico. Presenta errores de validación.</h3>';
+				$mensaje->contenido .= "Código: " .$resultado["codigo"] ."</br>Fecha de Respuesta:  " .$resultado["fechaRespuesta"] ."</br>Mensaje Validación:  ";
 
-				if ( !is_null( $resultado['mensajesValidacion'] ) )
-				{
-					if ( gettype( $resultado["mensajesValidacion"]->string ) == 'string' )
-					{
-						$mensaje->contenido .= "</br>" . $resultado["mensajesValidacion"]->string;
-					}else{
-						foreach ($resultado["mensajesValidacion"]->string as $key => $value) 
-						{
-							$mensaje->contenido .= "</br>" . $value;
-						}
-					}				
-				}
+				$mensaje->contenido .= "</br>" . $resultado["reglasNotificacionDIAN"];
     			break;
     	}
 
