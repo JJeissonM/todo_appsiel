6e42cb42338cc83042af014ce047f9941460d80f
Ricardo Garces <ricardo.garces@appsiel.com.co>
2021-08-09 15:49:04 -0500

    Certificados de Notas - Dos Escudos

--- appsiel/resources/views/core/dis_formatos/plantillas/certificado_notas_marca_agua.blade.php
+++ appsiel/resources/views/core/dis_formatos/plantillas/certificado_notas_marca_agua.blade.php
@@ -1,17 +1,20 @@
-<style>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+	<style>
 
 	img {
 		padding-left:30px;
 	}
-
 	
 	.page-break {
 		page-break-after: always;
 	}
-</style>
-
-<style>
-    @page { margin: 100px 25px; }
+    @page { margin: 40px 25px; }
     header { 
     	position: fixed; 
     	top: -60px; 
@@ -56,7 +59,9 @@
 		width: 95%;
 	}
 
- </style>
+	</style>
+</head>
+<body  style="font-size: 17px;">
 
 <?php    
     $colegio = App\Core\Colegio::where('empresa_id',Auth::user()->empresa_id)->get()->first();
@@ -66,114 +71,113 @@
 
 @foreach($estudiantes as $estudiante)
     
-	<div style="font-size: 17px;">
-		
 		<div class="watermark-{{$tam_hoja}} escudo">
 		    <img src="{{ $url }}"/>
 		</div> 
-
-		<div align="center" style="font-size: 1.1em;">
-			<b>{{ $colegio->descripcion }}</b><br/>
-			<b style="padding-top: -10px;">Educación básica primaria</b><br/>
-			Aprobado según resolución No. {{ $colegio->resolucion }}<br/>
-			{{ $colegio->ciudad }}<br/>
-		</div>
-
-		<br><br>
-
-		<div align="center">
-			EL DIRECTOR(A) GENERAL DE {{ $colegio->descripcion }}
-		</div>
-		
-		<br>
-
-		<div align="center">
-			CERTIFICA QUE:
-		</div>
-
-
-		<br>
-
-
-		<div align="center" style="font-weight: bold;">
-			{{ $estudiante->nombre_completo }}
-		</div>
-
-		<br>
-
-		<div style="text-align: justify;">
-			Cursó en esta institución educativa el grado <b>{{ $curso->grado->descripcion }}</b>, según pensum oficial. Habiendo obtenido en el {{ $periodo_lectivo->descripcion }} las calificaciones que a continuación se registran:
-		</div>		
-
-		<br>
-
-		<div class="container-fluid">
-			<div class="row">
-				{!! View::make( 'core.dis_formatos.plantillas.tabla_asignaturas_calificacion_2', compact( 'asignaturas','colegio','estudiante','curso', 'periodo_id' ) )->render() !!}
+<table width="100%">
+	<tr>
+		<td>
+			<img src="{{ $url }}" width="150px"/>
+		</td>
+		<td colspan="4">
+			<div align="center" style="font-size: 1.1em;">
+				<b>{{ $colegio->descripcion }}</b><br/>
+				<b style="padding-top: -10px;">Educación básica primaria</b><br/>
+				Aprobado según resolución No. {{ $colegio->resolucion }}<br/>
+				{{ $colegio->ciudad }}<br/>
 			</div>
-		</div>
-
-		<br>
-
-
-		<div style="text-align: justify;">
-			Observaciones: APROBÓ( &nbsp;&nbsp; )  &nbsp;&nbsp;&nbsp;&nbsp;    REPROBÓ( &nbsp;&nbsp; )    &nbsp;&nbsp;&nbsp;&nbsp;    APLAZÓ( &nbsp;&nbsp; )
+		</td>
+		<td>	
+		</td>
+	</tr>
+	<tr>
+		<td colspan="6">
 			<br>
-			{{ $observacion_adicional }}
-		</div>
-
-		
-		
-		<br><br>
-
-		<div style="text-align: justify;">
-			Para mayor constancia, se firma la presente en la ciudad de {{ $colegio->ciudad }} a los {{ $array_fecha[0] }} días del mes de {{ $array_fecha[1] }} de {{ $array_fecha[2] }}.
-		</div>
-
-
-		<br><br>
-	</div>
-
-	<div style="width: 100%;">
-		
-		<div style="float: left; width: 50%; text-align: center;">
-			<?php
-				$url_firma = '';
-				if ( $firma_autorizada_1->imagen != '' )
-				{
-					$url_firma = asset( config('configuracion.url_instancia_cliente') ).'/storage/app/firmas_autorizadas/'.$firma_autorizada_1->imagen;
-				}
-			?>
-			@if( $url_firma != '')
-				<img src="{{ $url_firma }}" width="250px" height="70px" style="left: 30px;position: absolute; z-index: 1;"/>
-			@else
-				_____________________________
-			@endif
-			<br><br><br>
-			{{ $firma_autorizada_1->tercero->descripcion }} 
+			<div align="center">
+				EL DIRECTOR(A) GENERAL DE {{ $colegio->descripcion }}
+			</div>		
 			<br>
-			{{ $firma_autorizada_1->titulo_tercero }} 
-		</div>
-
-		<div style="float: left; width: 50%; text-align: center;">
-			<?php
-				$url_firma = '';
-				if ( $firma_autorizada_2->imagen != '' )
-				{
-					$url_firma = asset( config('configuracion.url_instancia_cliente') ).'/storage/app/firmas_autorizadas/'.$firma_autorizada_2->imagen;
-				}
-			?>
-			@if( $url_firma != '')
-				<img src="{{ $url_firma }}" width="250px" height="70px" style="left: 30px;position: absolute; z-index: 1;"/>
-			@else
-				_____________________________
-			@endif
-			<br><br><br>
-			{{ $firma_autorizada_2->tercero->descripcion }} 
+			<div align="center">
+				CERTIFICA QUE:
+			</div>
 			<br>
-			{{ $firma_autorizada_2->titulo_tercero }} 
-		</div>
-	</div>
+			<div style="text-align: justify;">
+			<b>{{ $estudiante->nombre_completo }}</b>,
+			Cursó en esta institución educativa el grado <b>{{ $curso->grado->descripcion }}</b>, según pensum oficial. Habiendo obtenido en el {{ $periodo_lectivo->descripcion }} las calificaciones que a continuación se registran:
+			</div>		
 
-	<div class="page-break"></div>
+			<br>
+				
+		</td>
+	</tr>
+	{!! View::make( 'core.dis_formatos.plantillas.tabla_asignaturas_calificacion_2', compact( 'asignaturas','colegio','estudiante','curso', 'periodo_id' ) )->render() !!}	
+	<tr>
+		<td colspan="6">
+				<br>
+			<div style="text-align: justify;">
+				Observaciones: APROBÓ( &nbsp;&nbsp; )  &nbsp;&nbsp;&nbsp;&nbsp;    REPROBÓ( &nbsp;&nbsp; )    &nbsp;&nbsp;&nbsp;&nbsp;    APLAZÓ( &nbsp;&nbsp; )
+				<br>
+				{{ $observacion_adicional }}
+			</div>
+			<br><br>
+			<div style="text-align: justify;">
+				Para mayor constancia, se firma la presente en la ciudad de {{ $colegio->ciudad }} a los {{ $array_fecha[0] }} días del mes de {{ $array_fecha[1] }} de {{ $array_fecha[2] }}.
+			</div>
+			<br><br>			
+		</td>
+	</tr>
+	<tr>
+		<td colspan="6">
+		<div style="width: 100%;">
+			
+			<div style="float: left; width: 50%; text-align: center;">
+				<?php
+					$url_firma = '';
+					/*if ( $firma_autorizada_1->imagen != '' )
+					{
+						$url_firma = asset( config('configuracion.url_instancia_cliente') ).'/storage/app/firmas_autorizadas/'.$firma_autorizada_1->imagen;
+					}*/
+				?>
+				@if( $url_firma != '')
+					<img src="{{ $url_firma }}" width="250px" height="70px" style="left: 30px;position: absolute; z-index: 1;"/>
+				@else
+					_____________________________
+				@endif
+				<br><br><br>
+				{{ '$firma_autorizada_1->tercero->descripcion' }} 
+				<br>
+				<!--{ { $firma_autorizada_1->titulo_tercero }} -->
+			</div>
+
+			<div style="float: left; width: 50%; text-align: center;">
+				<?php
+					$url_firma = '';
+					/*if ( $firma_autorizada_2->imagen != '' )
+					{
+						$url_firma = asset( config('configuracion.url_instancia_cliente') ).'/storage/app/firmas_autorizadas/'.$firma_autorizada_2->imagen;
+					}*/
+				?>
+				@if( $url_firma != '')
+					<img src="{{ $url_firma }}" width="250px" height="70px" style="left: 30px;position: absolute; z-index: 1;"/>
+				@else
+					_____________________________
+				@endif
+				<br><br><br>
+				{{ '$firma_autorizada_2->tercero->descripcion' }} 
+				<br>
+				<!--{ { $firma_autorizada_2->titulo_tercero }}-->
+			</div>
+			
+		</div>
+		
+		<div style="clear: both"></div>
+		<div style="text-align: center">Dirección: {{ $colegio->direccion }} Celular: {{ $colegio->telefonos }}</div>
+		<div style="text-align: center">{{ $colegio->ciudad }}</div>
+
+		<div class="page-break"></div>	
+		</td>
+	</tr>
+</table>		
 @endforeach
+</body>
+</html>
--- appsiel/resources/views/core/dis_formatos/plantillas/tabla_asignaturas_calificacion_2.blade.php
+++ appsiel/resources/views/core/dis_formatos/plantillas/tabla_asignaturas_calificacion_2.blade.php
@@ -1,21 +1,18 @@
-<table class="table table-bordered contenido" align="center">
-    <thead>
+
         <tr>
-            <th style="border: 1px solid grey;background-color: #E0E0E0; width:400px;">Asignaturas</th>
-            <th style="border: 1px solid grey;background-color: #E0E0E0; width:30px;">I.H.</th>
-            <th colspan="3" style="border: 1px solid grey;background-color: #E0E0E0;">Calificaciones</th>
+            <td  colspan="2" style="border: 1px solid grey;background-color: #E0E0E0; width:400px;">Asignaturas</td>
+            <td style="border: 1px solid grey;background-color: #E0E0E0; width:30px;">I.H.</td>
+            <td colspan="3" style="border: 1px solid grey;background-color: #E0E0E0;">Calificaciones</td>
         </tr>
-    </thead>
-    <tbody>
     @foreach($asignaturas as $asignatura)
 
         <?php
 
             $calificacion = App\Calificaciones\Calificacion::get_la_calificacion( $periodo_id, $curso->id, $estudiante->id, $asignatura->id);
         ?>
-
-        <tr style="font-size: 15px;">
-            <td style="border: 1px solid grey; width:400px;">
+        
+        <tr style="font-size: 15px;" >
+            <td colspan="2" style="border: 1px solid grey; width:400px;">
                 {{ $asignatura->descripcion }}
             </td>
             <td align="center" style="border: 1px solid grey; width:30px;">
@@ -31,6 +28,4 @@
                 {{ $calificacion->escala_nacional }}
             </td>
         </tr>
-    @endforeach       
-    </tbody>
-</table>
+    @endforeach   
