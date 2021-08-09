1d38109bb53e83f2c3ebbd235092217d2f420d18
Ricardo Garces <ricardo.garces@appsiel.com.co>
2021-08-09 15:57:01 -0500

    Correcion Comentarios  Certificado de Notas

--- appsiel/resources/views/core/dis_formatos/plantillas/certificado_notas_marca_agua.blade.php
+++ appsiel/resources/views/core/dis_formatos/plantillas/certificado_notas_marca_agua.blade.php
@@ -133,10 +133,10 @@
 			<div style="float: left; width: 50%; text-align: center;">
 				<?php
 					$url_firma = '';
-					/*if ( $firma_autorizada_1->imagen != '' )
+					if ( $firma_autorizada_1->imagen != '' )
 					{
 						$url_firma = asset( config('configuracion.url_instancia_cliente') ).'/storage/app/firmas_autorizadas/'.$firma_autorizada_1->imagen;
-					}*/
+					}
 				?>
 				@if( $url_firma != '')
 					<img src="{{ $url_firma }}" width="250px" height="70px" style="left: 30px;position: absolute; z-index: 1;"/>
@@ -146,16 +146,16 @@
 				<br><br><br>
 				{{ '$firma_autorizada_1->tercero->descripcion' }} 
 				<br>
-				<!--{ { $firma_autorizada_1->titulo_tercero }} -->
+				{{ $firma_autorizada_1->titulo_tercero }}
 			</div>
 
 			<div style="float: left; width: 50%; text-align: center;">
 				<?php
 					$url_firma = '';
-					/*if ( $firma_autorizada_2->imagen != '' )
+					if ( $firma_autorizada_2->imagen != '' )
 					{
 						$url_firma = asset( config('configuracion.url_instancia_cliente') ).'/storage/app/firmas_autorizadas/'.$firma_autorizada_2->imagen;
-					}*/
+					}
 				?>
 				@if( $url_firma != '')
 					<img src="{{ $url_firma }}" width="250px" height="70px" style="left: 30px;position: absolute; z-index: 1;"/>
@@ -165,7 +165,7 @@
 				<br><br><br>
 				{{ '$firma_autorizada_2->tercero->descripcion' }} 
 				<br>
-				<!--{ { $firma_autorizada_2->titulo_tercero }}-->
+				{{ $firma_autorizada_2->titulo_tercero }}
 			</div>
 			
 		</div>
