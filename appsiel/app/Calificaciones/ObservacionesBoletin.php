44e0766683ef965a12f113511dd88b464beeaf37
Adalberto Perez <ing.adalbertoperez@gmail.com>
2021-09-30 03:39:16 -0500

    Calif. corregir error Whoops Consulta observacion

--- appsiel/app/Calificaciones/ObservacionesBoletin.php
+++ appsiel/app/Calificaciones/ObservacionesBoletin.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 use DB;
 use Auth;
 
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
+
 class ObservacionesBoletin extends Model
 {
     protected $table = 'sga_observaciones_boletines';
@@ -65,7 +68,7 @@ class ObservacionesBoletin extends Model
 
     public static function consultar_registros_director_grupo($nro_registros, $search)
     {
-        return ObservacionesBoletin::leftJoin('sga_estudiantes', 'sga_estudiantes.id', '=', 'sga_observaciones_boletines.id_estudiante')
+        $collection = ObservacionesBoletin::leftJoin('sga_estudiantes', 'sga_estudiantes.id', '=', 'sga_observaciones_boletines.id_estudiante')
             ->leftJoin('core_terceros', 'core_terceros.id', '=', 'sga_estudiantes.core_tercero_id')
             ->leftJoin('sga_cursos', 'sga_cursos.id', '=', 'sga_observaciones_boletines.curso_id')
             ->leftJoin('sga_periodos', 'sga_periodos.id', '=', 'sga_observaciones_boletines.id_periodo')
@@ -80,14 +83,68 @@ class ObservacionesBoletin extends Model
                 'sga_observaciones_boletines.puesto AS campo5',
                 'sga_observaciones_boletines.observacion AS campo6',
                 'sga_observaciones_boletines.id AS campo7'
-            )->orWhere("sga_periodos_lectivos.descripcion", "LIKE", "%$search%")
-            ->orWhere("sga_periodos.descripcion", "LIKE", "%$search%")
-            ->orWhere("sga_cursos.descripcion", "LIKE", "%$search%")
-            ->orWhere(DB::raw('CONCAT(core_terceros.apellido1," ",core_terceros.apellido2," ",core_terceros.nombre1," ",core_terceros.otros_nombres)'), "LIKE", "%$search%")
-            ->orWhere("sga_observaciones_boletines.puesto", "LIKE", "%$search%")
-            ->orWhere("sga_observaciones_boletines.observacion", "LIKE", "%$search%")
+            )
             ->orderBy('sga_observaciones_boletines.created_at', 'DESC')
-            ->paginate($nro_registros);
+            ->get();
+
+        //hacemos el filtro de $search si $search tiene contenido
+        $nuevaColeccion = [];
+        if (count($collection) > 0) {
+            if (strlen($search) > 0) {
+                $nuevaColeccion = $collection->filter(function ($c) use ($search) {
+                    if (self::likePhp([$c->campo1, $c->campo2, $c->campo3, $c->campo4, $c->campo5, $c->campo6, $c->campo7], $search)) {
+                        return $c;
+                    }
+                });
+            } else {
+                $nuevaColeccion = $collection;
+            }
+        }
+
+        $request = request(); //obtenemos el Request para obtener la url y la query builder
+
+        if (empty($nuevaColeccion)) {
+            return $array = new LengthAwarePaginator([], 1, 1, 1, [
+                'path' => $request->url(),
+                'query' => $request->query(),
+            ]);
+        }
+
+        //obtenemos el numero de la página actual, por defecto 1
+        $page = 1;
+        if (isset($_GET['page'])) {
+            $page = $_GET['page'];
+        }
+        $total = count($nuevaColeccion); //Total para contar los registros mostrados
+        $starting_point = ($page * $nro_registros) - $nro_registros; // punto de inicio para mostrar registros
+        $array = $nuevaColeccion->slice($starting_point, $nro_registros); //indicamos desde donde y cuantos registros mostrar
+        $array = new LengthAwarePaginator($array, $total, $nro_registros, $page, [
+            'path' => $request->url(),
+            'query' => $request->query(),
+        ]); //finalmente se pagina y organiza la coleccion a devolver con todos los datos
+
+        return $array;
+    }
+
+    /**
+     * SQL Like operator in PHP.
+     * Returns TRUE if match else FALSE.
+     * @param array $valores_campos_seleccionados de campos donde se busca
+     * @param string $searchTerm termino de busqueda
+     * @return bool
+     */
+    public static function likePhp($valores_campos_seleccionados, $searchTerm)
+    {
+        $encontrado = false;
+        $searchTerm = str_slug($searchTerm); // Para eliminar acentos
+        foreach ($valores_campos_seleccionados as $valor_campo) {
+            $str = str_slug($valor_campo);
+            $pos = strpos($str, $searchTerm);
+            if ($pos !== false) {
+                $encontrado = true;
+            }
+        }
+        return $encontrado;
     }
 
     public static function sqlString_registros_director_grupo($search)
