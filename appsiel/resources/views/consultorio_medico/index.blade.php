f4be1154e656ef9b56783d2e914f20ef96dee863
Adalberto Perez <ing.adalbertoperez@gmail.com>
2021-12-17 11:12:46 -0500

    Consult. medico mejora vista index

--- appsiel/resources/views/consultorio_medico/index.blade.php
+++ appsiel/resources/views/consultorio_medico/index.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.principal')
 
+<?php 
+	$ultimas_consultas = App\Salud\ConsultaMedica::orderBy('id','DESC')->take(10)->get();
+	//dd($ultimas_consultas);
+?>
+
 @section('content')
 	{{ Form::bsMigaPan($miga_pan) }}
 	{!! $select_crear !!}
@@ -10,6 +15,45 @@
 	<div class="container-fluid">
 		<div class="marco_formulario">
 			
+			<div class="row">
+				<div class="col-md-4">
+					<h5>Últimos pacientes atendidos</h5>
+					<hr>
+					<table class="table table-bordered table-striped">
+						<thead>
+							<tr>
+								<th>Fecha consulta</th>
+								<th>Paciente</th>
+								<th>Atendido por</th>
+							</tr>
+						</thead>
+						<tbody>
+						@foreach($ultimas_consultas as $consulta)
+							
+								<tr>
+									<td>{{$consulta->fecha}}</td>
+									<td>
+										<a href="{{url('consultorio_medico/pacientes/') . '/' . $consulta->paciente_id . '?id=18&id_modelo=95&id_transaccion='}}">
+											{{$consulta->paciente->tercero->descripcion}}
+										</a>
+									</td>
+									<td>{{$consulta->profesional_salud->tercero->descripcion}}</td>
+								</tr>
+						@endforeach
+						</tbody>
+					</table>
+				</div>
+				<div class="col-md-4">
+					
+				</div>
+				<div class="col-md-4">
+					
+				</div>
+				<div class="col-md-4">
+					
+				</div>
+			</div>
+
 		</div>
 	</div>
 
