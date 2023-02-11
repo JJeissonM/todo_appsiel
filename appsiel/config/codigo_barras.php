dad07d7f8b7de295216988ea9435c975f87f557e
Adalberto Perez <ing.adalbertoperez@gmail.com>
2023-02-11 08:25:15 -0500

    .gitignore is now working

--- appsiel/config/codigo_barras.php
+++
@@ -1,8 +0,0 @@
-<?php
- return array (
-  'titulo' => '',
-  'longitud_item' => '5',
-  'longitud_talla' => '2',
-  'longitud_color' => '0',
-  'caracter_relleno' => '0'
-) ;
--- appsiel/config/gestion_de_cobros.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-
-    'lbl_formato_cxc' => 'Después del 30 se generan intereses de Mora a la Tasa Máxima legal vigente.<br/>
-        Favor consignar en la datos_cuenta_bancaria. A nombre de nombre_empresa. <br/>
-        FAVOR ENVIAR EL SOPORTE DE PAGO AL EMAIL: email_empresa',
-
-];
