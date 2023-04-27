e4ae2b16b4eef819d6cc14e76e6eb653456f2ad5
Adalberto Perez <ing.adalbertoperez@gmail.com>
2023-04-27 11:15:59 -0500

    .

--- appsiel/resources/views/nomina/nomina_electronica/pdf_base64_show.blade.php
+++ appsiel/resources/views/nomina/nomina_electronica/pdf_base64_show.blade.php
@@ -4,7 +4,8 @@
         <title> {{ 'Doc. Soporte Nomina Electronia ' . $encabezado_doc->tipo_documento_app->prefijo . $encabezado_doc->consecutivo }} </title>
     </head>
     <body>
-        <embed src="data:application/pdf;base64,{{$documento_electronico}}" type="application/pdf" width="100%" height="100%"/>
+        
+        <embed src="data:application/pdf;base64,{{$documento_electronico}}" type="application/pdf" width="100%" height="600px"/>
         
     </body>
 </html>
