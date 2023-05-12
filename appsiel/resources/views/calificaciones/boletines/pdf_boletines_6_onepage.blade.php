90253af173964eafb10efddfee783aee71f5a7dd
Adalberto Perez <ing.adalbertoperez@gmail.com>
2023-05-12 04:50:22 -0500

    idem

--- appsiel/resources/views/calificaciones/boletines/pdf_boletines_6_onepage.blade.php
+++ appsiel/resources/views/calificaciones/boletines/pdf_boletines_6_onepage.blade.php
@@ -27,7 +27,7 @@
 
     <h4 style="text-align: center; padding: 10px;">INFORME {{$lbl_numero_periodo}} PERIODO AÑO LECTIVO {{ explode( "-", $periodo->fecha_desde )[0] }}</h4>
     
-    <p style="padding-left: 30px;">
+    <p style="padding-left: auto;">
         <table style="width:100%;">
             <tr>
                 <td><b>FULL NAME:</b></td>
