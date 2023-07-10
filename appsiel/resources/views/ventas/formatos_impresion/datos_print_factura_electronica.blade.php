dcc9408100ade059eed578698e231e03a165e776
Adalberto Perez <ing.adalbertoperez@gmail.com>
2023-07-10 12:59:31 -0500

    idem

--- appsiel/resources/views/ventas/formatos_impresion/datos_print_factura_electronica.blade.php
+++ appsiel/resources/views/ventas/formatos_impresion/datos_print_factura_electronica.blade.php
@@ -8,6 +8,6 @@
     <b>CUFE: &nbsp;&nbsp;</b> {{ $documento_electronico->cufe }}
     <br>
     <p style="width: 100%; text-align: center;">
-        <img style="height: 200px; display: inline;" src="data:image/png;base64,{{DNS2D::getBarcodePNG($documento_electronico->qrcode, 'QRCODE')}}" alt="barcode" />
+        <img style="height: 150px; display: inline;" src="data:image/png;base64,{{DNS2D::getBarcodePNG($documento_electronico->qrcode, 'QRCODE')}}" alt="barcode" />
     </p>
 @endif
