3f76c7c369688967648e4dc31d60da693a05a45b
Adalberto Perez <ing.adalbertoperez@gmail.com>
2024-04-29 15:20:12 -0500

    idem

--- appsiel/app/AcademicoDocente/AsignacionProfesor.php
+++ appsiel/app/AcademicoDocente/AsignacionProfesor.php
@@ -156,7 +156,7 @@ class AsignacionProfesor extends Model
         
         $select = '<option value="">Seleccionar... </option>';
         foreach ($opciones as $opcion){
-            $select.='<option value="'.$opcion->id.'">'.$opcion->descripcion.'</option>';
+            $select.='<option value="'.$opcion->id.'">'.$opcion->descripcion. ' (' . $opcion->area->descripcion . ') </option>';
         }
 
         return $select;
