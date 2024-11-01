4b918203519dd2eb0030431dd1a0af4ba753f6e2
BackendBoss <backend@appsiel.com.co>
2024-11-01 05:53:57 -0500

    inv mejora impresion codigos de barras en hoja carta

--- appsiel/app/FacturacionElectronica/DATAICO/FacturaGeneral.php
+++ appsiel/app/FacturacionElectronica/DATAICO/FacturaGeneral.php
@@ -248,8 +248,20 @@ class FacturaGeneral
           * Se envia el precio unitario sin haber quitado el decuento. DATAICO hace el calculo de descuento en su plataforma con base en discount_rate.
           */
          $price = $linea->precio_unitario / (1 + $linea->tasa_impuesto / 100);
+         $original_price = 0;
+         if ($price == 0) { // Obsequio o Precio cero
+            $price = $linea->item->get_costo_promedio() * (1 + 10 / 100);
+
+            $original_price = $price;
+
+            $linea->tasa_descuento = 100;
+         }
 
          $string_items .= '{"sku": "' . $linea->item->id . '","description": "' . str_replace('"', '\"', $linea->item->descripcion) . '","quantity": ' . abs( number_format( $linea->cantidad, $this->cantidadDecimales, '.', '') ) . ',"price": ' . abs( number_format( $price, $this->cantidadDecimales, '.', '') );
+         
+         if ( $original_price != 0 ) {
+            $string_items .= ',"original_price": ' . $original_price;
+         }
 
          if ( $linea->tasa_descuento != 0 )
          {
--- appsiel/resources/views/inventarios/reportes/una_etiqueta_codigo_barras.blade.php
+++ appsiel/resources/views/inventarios/reportes/una_etiqueta_codigo_barras.blade.php
@@ -24,6 +24,10 @@
 
         $ancho_codigo = 2;
         $alto_codigo = 100;
+        if ( $numero_columnas == 4) {
+            $ancho_codigo = 1.35;
+            $alto_codigo = 80;
+        }
 
         if ($ancho != '') {
             $ancho_codigo = $ancho / 100;
@@ -36,12 +40,13 @@
         DNS1D::getBarcodePNG( texto_codigo, tipo_codigo, ancho, alto) 
         tipo_codigo: { C128B, C39 }
     -->
-
-    <p>
+    <p style="margin-left: -26px;">
         <!-- Solo se envian los 12 primeros digitos, la function getBarcodePNG dibuja el codigo de barras con el digito de control al final -->
-        <img src="data:image/png;base64,{{ DNS1D::getBarcodePNG( substr($codigo_barras,0,12), "EAN13", $ancho_codigo, $alto_codigo) }}" alt="barcode" />
+        <img src="data:image/png;base64,{{ DNS1D::getBarcodePNG( substr($codigo_barras,0,12), "EAN13", $ancho_codigo, $alto_codigo) }}" alt="barcode"/>
     </p>
-    {{ $codigo_barras }}
+    <p>
+        {{ $codigo_barras . $fila->get_codigo_proveedor() }}
+    </p>
 </div>
 
 
