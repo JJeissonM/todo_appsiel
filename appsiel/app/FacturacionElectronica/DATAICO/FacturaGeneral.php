310f8147bfd3c4b81a9ba557cd53bb229e76f8a1
BackendBoss <backend@appsiel.com.co>
2025-02-07 16:48:48 -0500

    feat: handle payment date fallback for invoices with invalid vencimiento date

--- appsiel/app/FacturacionElectronica/DATAICO/FacturaGeneral.php
+++ appsiel/app/FacturacionElectronica/DATAICO/FacturaGeneral.php
@@ -177,7 +177,12 @@ class FacturaGeneral
          }
       }
       
-      return '"env": "' . $this->env . '","dataico_account_id": "' . config('facturacion_electronica.tokenEmpresa') . '","number":'.$this->doc_encabezado->consecutivo.',"issue_date": "' . date_format( date_create( $this->doc_encabezado->fecha ),'d/m/Y') . '","payment_date": "' . date_format( date_create( $this->doc_encabezado->fecha_vencimiento ),'d/m/Y') . '","invoice_type_code": "' . $this->invoice_type_code . '","payment_means_type": "' . $payment_means_type . '","payment_means": "' . $payment_means . '","numbering":{"resolution_number":"' . $resolucion->numero_resolucion . '","prefix":"' . $resolucion->prefijo . '","flexible":' . $flexible . '},"notes":["' . $notes2 . '"], "customer": ' . $this->get_datos_cliente();
+      $fecha_vencimiento = $this->doc_encabezado->fecha_vencimiento;
+      if (explode('-', $fecha_vencimiento)[0] == '0000') {         
+         $fecha_vencimiento = $this->doc_encabezado->fecha;
+      }
+
+      return '"env": "' . $this->env . '","dataico_account_id": "' . config('facturacion_electronica.tokenEmpresa') . '","number":'.$this->doc_encabezado->consecutivo.',"issue_date": "' . date_format( date_create( $this->doc_encabezado->fecha ),'d/m/Y') . '","payment_date": "' . date_format( date_create( $fecha_vencimiento ),'d/m/Y') . '","invoice_type_code": "' . $this->invoice_type_code . '","payment_means_type": "' . $payment_means_type . '","payment_means": "' . $payment_means . '","numbering":{"resolution_number":"' . $resolucion->numero_resolucion . '","prefix":"' . $resolucion->prefijo . '","flexible":' . $flexible . '},"notes":["' . $notes2 . '"], "customer": ' . $this->get_datos_cliente();
    }
 
    public function get_encabezado_nota_credito( $factura_doc_encabezado )
