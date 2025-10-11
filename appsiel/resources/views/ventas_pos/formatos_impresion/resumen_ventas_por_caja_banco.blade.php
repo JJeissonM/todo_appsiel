2e823ca0939b5be0400b9f27f6e97ef09cc6ae68
BackendBoss <backend@appsiel.com.co>
2025-10-11 06:17:00 -0500

    idem

--- appsiel/resources/views/ventas_pos/formatos_impresion/resumen_ventas_por_caja_banco.blade.php
+++ appsiel/resources/views/ventas_pos/formatos_impresion/resumen_ventas_por_caja_banco.blade.php
@@ -1,39 +1,44 @@
-<div style="border: solid 1px #ddd; border-radius: 4px; padding: 10px;">
-    <h5>RESUMEN POR MEDIOS DE PAGO</h5>
-    <table style=" font-size: {{ $tamanino_fuente_2 }}; width=50%; ">
+<table width="100%" style=" font-size: {{ $tamanino_fuente_2 }};">
+        <thead>
+            <tr>
+                <th colspan="5">RESUMEN POR MEDIOS DE PAGO</th>
+            </tr>
+        </thead>
         <tbody>
             <?php 
                 $total_ventas_al_detal = 0;
             ?>
             @foreach($ventas_por_medios_pago_con_iva as $linea)
                 <tr>
-                    <td>
+                    <td colspan="3" align="right">
                         <b>{{ $linea->caja_banco }}:</b>
                     </td>
                     <td align="right">
                         ${{ number_format( $ventas_contado_sin_iva * $linea->porcentaje_participacion_total_ventas, 0, ',', '.') }}
                     </td>
+                    <td></td>
                 </tr>
                 <?php 
                     $total_ventas_al_detal += $ventas_contado_sin_iva * $linea->porcentaje_participacion_total_ventas;
                 ?>
             @endforeach
             <tr>
-                <td align="right">
+                <td colspan="3" align="right" style="width:40%;">
                     <b>Crédito:</b>
                 </td>
-                <td align="right">
+                <td align="right" style="width:20%;">
                     ${{ number_format( $ventas_credito_sin_iva, 0, ',', '.') }}
                 </td>
+                <td></td>
             </tr>
             <tr>
-                <td align="right">
-                    <b>VENTAS AL DETAL:</b>
+                <td colspan="3" align="right">
+                    <b>TOTAL:</b>
                 </td>
                 <td align="right">
                     ${{ number_format( $total_ventas_al_detal + $ventas_credito_sin_iva, 0, ',', '.' ) }}
                 </td>
+                <td></td>
             </tr>
         </tbody>
     </table>
-</div>
