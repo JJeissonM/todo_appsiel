8235d9e1922acc3e3759f18211d1809e5af24c14
Adalberto Perez <ing.adalbertoperez@gmail.com>
2026-02-09 11:27:17 -0500

    fix index proveedores

--- appsiel/app/Compras/Proveedor.php
+++ appsiel/app/Compras/Proveedor.php
@@ -13,7 +13,7 @@ class Proveedor extends Model
 
     protected $fillable = ['core_tercero_id', 'clase_proveedor_id', 'inv_bodega_id', 'liquida_impuestos', 'condicion_pago_id', 'codigo', 'estado'];
 
-    public $encabezado_tabla = ['<i style="font-size: 20px;" class="fa fa-check-square-o"></i>', 'Identificación', 'Tercero', 'Dirección', 'Teléfono', 'Clase de proveedor', 'Liquida impuestos', 'Condición de pago', 'Estado'];
+    public $encabezado_tabla = ['<i style="font-size: 20px;" class="fa fa-check-square-o"></i>', 'Identificación', 'Tercero',  'Establecimiento', 'Dirección', 'Teléfono', 'Clase de proveedor', 'Liquida impuestos', 'Condición de pago', 'Estado'];
 
     public $urls_acciones = '{"eliminar":"web_eliminar/id_fila"}';
 
@@ -28,16 +28,18 @@ class Proveedor extends Model
             ->select(
                 'core_terceros.numero_identificacion AS campo1',
                 'core_terceros.descripcion AS campo2',
-                'core_terceros.direccion1 AS campo3',
-                'core_terceros.telefono1 AS campo4',
-                'compras_clases_proveedores.descripcion AS campo5',
-                'compras_proveedores.liquida_impuestos AS campo6',
-                'compras_condiciones_pago.descripcion AS campo7',
-                'compras_proveedores.estado AS campo8',
-                'compras_proveedores.id AS campo9'
+                'core_terceros.razon_social AS campo3',
+                'core_terceros.direccion1 AS campo4',
+                'core_terceros.telefono1 AS campo5',
+                'compras_clases_proveedores.descripcion AS campo6',
+                'compras_proveedores.liquida_impuestos AS campo7',
+                'compras_condiciones_pago.descripcion AS campo8',
+                'compras_proveedores.estado AS campo9',
+                'compras_proveedores.id AS campo10'
             )
             ->where("core_terceros.numero_identificacion", "LIKE", "%$search%")
             ->orWhere("core_terceros.descripcion", "LIKE", "%$search%")
+            ->orWhere("core_terceros.razon_social", "LIKE", "%$search%")
             ->orWhere("core_terceros.direccion1", "LIKE", "%$search%")
             ->orWhere("core_terceros.telefono1", "LIKE", "%$search%")
             ->orWhere("compras_clases_proveedores.descripcion", "LIKE", "%$search%")
@@ -54,6 +56,7 @@ class Proveedor extends Model
             ->select(
                 'core_terceros.numero_identificacion AS IDENTIFICACIÓN',
                 'core_terceros.descripcion AS TERCERO',
+                'core_terceros.razon_social AS RAZÓN_SOCIAL',
                 'core_terceros.direccion1 AS DIRECCIÓN',
                 'core_terceros.telefono1 AS TELÉFONO',
                 'compras_clases_proveedores.descripcion AS CLASE_DE_PROVEEDOR',
