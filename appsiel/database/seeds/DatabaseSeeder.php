9a41abec100a3f4a6a870880d2c1e413f1b40098
Adalberto Perez <ing.adalbertoperez@gmail.com>
2026-03-08 15:56:44 -0500

    fix seeders

--- appsiel/database/seeds/DatabaseSeeder.php
+++ appsiel/database/seeds/DatabaseSeeder.php
@@ -11,22 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(IcfesQuestionBankSeeder::class);
-        $this->call(CumplimientoGuiasReporteSeeder::class);
-        if (env('APPSIEL_CLIENTE') === 'SIESA') {
-            $this->call(SiesaListasDescuentosSeeder::class);
-            $this->call(SiesaClientesSeeder::class);
-            $this->call(SiesaEncabezadosListasDescuentosSeeder::class);
-            $this->call(SiesaProveedoresEnterpriseSeeder::class);
-            $this->call(SiesaImpuestosSeeder::class);
-            $this->call(SiesaRetencionesSeeder::class);
-            $this->call(SiesaDatosCompletosProveedoresSeeder::class);
-        }
-        $this->call(NominaActualizacionSueldosSeeder::class);
         $this->call(ProveedorCuentasBancariasSeeder::class);
         $this->call(TesoreriaChequerasPermissionSeeder::class);
         $this->call(ComprasRetencionesLineaSeeder::class);
-        $this->call(NominaCotizante51Seeder::class);
     }
 }
 
