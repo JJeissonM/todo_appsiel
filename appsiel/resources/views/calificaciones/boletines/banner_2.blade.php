10c162524aebf9ec4946429bf739a01484fbf010
Adalberto Pérez Oliveros <ing.adalbertoperez@gmail.com>
2026-04-16 12:40:02 -0500

    idem

--- appsiel/resources/views/calificaciones/boletines/banner_2.blade.php
+++ appsiel/resources/views/calificaciones/boletines/banner_2.blade.php
@@ -9,9 +9,9 @@
         <td align="center" @if( isset($centrar_banner_en_hoja) && $centrar_banner_en_hoja ) width="50%" @endif>
             <b style="font-size: {{$tam_letra}}mm;">{{ $colegio->descripcion }}</b>
             <br/>
-            <b style="font-size: {{$tam_letra - 0.7}}mm;">{!! $colegio->slogan !!}</b>
+            <b style="font-size: {{$tam_letra - 0.5}}mm;">{!! $colegio->slogan !!}</b>
             <br/>
-            <span style="font-size: {{$tam_letra - 0.7}}mm;">
+            <span style="font-size: {{$tam_letra - 0.5}}mm;">
                 Resolución No. {{ $colegio->resolucion }}<br/>
                 {{ $colegio->direccion }}, {{ $colegio->ciudad }}, Teléfono: {{ $colegio->telefonos }}
             </span>
