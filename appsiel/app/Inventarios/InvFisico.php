5ad0d891116c8fe42a841cf5c1ad5e72c8d28101
Adalberto Perez <ing.adalbertoperez@gmail.com>
2026-05-20 09:08:42 -0500

    feat: vista índice inventario físico - permitir visualizar solo los documentos creados por el mismo usuario cuando no sea administrador.

--- appsiel/app/Inventarios/InvFisico.php
+++ appsiel/app/Inventarios/InvFisico.php
@@ -16,77 +16,69 @@ class InvFisico extends InvDocEncabezado
 
     public static function consultar_registros($nro_registros, $search)
     {
-        $core_tipo_transaccion_id = 27; // Invetario Físico
+        $query = self::queryConsultarRegistros();
 
-        if ( $search == '' )
+        if ( $search != '' )
         {
-            return InvDocEncabezado::leftJoin('core_tipos_docs_apps', 'core_tipos_docs_apps.id', '=', 'inv_doc_encabezados.core_tipo_doc_app_id')
-                                ->leftJoin('core_terceros', 'core_terceros.id', '=', 'inv_doc_encabezados.core_tercero_id')
-                                ->leftJoin('inv_bodegas', 'inv_bodegas.id', '=', 'inv_doc_encabezados.inv_bodega_id')
-                                ->where('inv_doc_encabezados.core_tipo_transaccion_id', $core_tipo_transaccion_id)
-                                ->where('inv_doc_encabezados.core_empresa_id', Auth::user()->empresa_id)
-                                ->select(
-                                    DB::raw('DATE_FORMAT(inv_doc_encabezados.fecha,"%d-%m-%Y") AS campo1'),
-                                    DB::raw( 'CONCAT(core_tipos_docs_apps.prefijo," ",inv_doc_encabezados.consecutivo) AS campo2' ),
-                                    'inv_bodegas.descripcion AS campo3',
-                                    DB::raw( 'CONCAT(core_terceros.descripcion," (",core_terceros.razon_social,")") AS campo4' ),
-                                    'inv_doc_encabezados.descripcion AS campo5',
-                                    'inv_doc_encabezados.estado AS campo6',
-                                    'inv_doc_encabezados.id AS campo7'
-                                )
-                                ->orderBy('inv_doc_encabezados.created_at', 'DESC')
-                                ->paginate($nro_registros);
+            $query->where(function ($query) use ($search) {
+                $query->where("inv_doc_encabezados.fecha", "LIKE", "%$search%")
+                    ->orWhere(DB::raw('CONCAT(core_tipos_docs_apps.prefijo," ",inv_doc_encabezados.consecutivo)'), "LIKE", "%$search%")
+                    ->orWhere("inv_bodegas.descripcion", "LIKE", "%$search%")
+                    ->orWhere(DB::raw('CONCAT(core_terceros.descripcion," (",core_terceros.razon_social,")")'), "LIKE", "%$search%")
+                    ->orWhere("inv_doc_encabezados.descripcion", "LIKE", "%$search%")
+                    ->orWhere("inv_doc_encabezados.estado", "LIKE", "%$search%");
+            });
         }
 
-        return InvDocEncabezado::leftJoin('core_tipos_docs_apps', 'core_tipos_docs_apps.id', '=', 'inv_doc_encabezados.core_tipo_doc_app_id')
-            ->leftJoin('core_terceros', 'core_terceros.id', '=', 'inv_doc_encabezados.core_tercero_id')
-            ->leftJoin('inv_bodegas', 'inv_bodegas.id', '=', 'inv_doc_encabezados.inv_bodega_id')
-            ->where('inv_doc_encabezados.core_tipo_transaccion_id', $core_tipo_transaccion_id)
-            ->where('inv_doc_encabezados.core_empresa_id', Auth::user()->empresa_id)
-            ->select(
-                DB::raw('DATE_FORMAT(inv_doc_encabezados.fecha,"%d-%m-%Y") AS campo1'),
-                DB::raw( 'CONCAT(core_tipos_docs_apps.prefijo," ",inv_doc_encabezados.consecutivo) AS campo2' ),
-                'inv_bodegas.descripcion AS campo3',
-                DB::raw( 'CONCAT(core_terceros.descripcion," (",core_terceros.razon_social,")") AS campo4' ),
-                'inv_doc_encabezados.descripcion AS campo5',
-                'inv_doc_encabezados.estado AS campo6',
-                'inv_doc_encabezados.id AS campo7'
-            )
-            ->where("inv_doc_encabezados.fecha", "LIKE", "%$search%")
-            ->orWhere(DB::raw('CONCAT(core_tipos_docs_apps.prefijo," ",inv_doc_encabezados.consecutivo)'), "LIKE", "%$search%")
-            ->orWhere("inv_bodegas.descripcion", "LIKE", "%$search%")
-            ->orWhere(DB::raw('CONCAT(core_terceros.descripcion," (",core_terceros.razon_social,")")'), "LIKE", "%$search%")
-            ->orWhere("inv_doc_encabezados.descripcion", "LIKE", "%$search%")
-            ->orWhere("inv_doc_encabezados.estado", "LIKE", "%$search%")
+        return $query
             ->orderBy('inv_doc_encabezados.created_at', 'DESC')
             ->paginate($nro_registros);
     }
     public static function sqlString($search)
     {
-        $core_tipo_transaccion_id = 27; // Invetario Físico
+        $string = self::queryConsultarRegistros([
+                DB::raw('DATE_FORMAT(inv_doc_encabezados.fecha,"%d-%m-%Y") AS FECHA'),
+                DB::raw('CONCAT(core_tipos_docs_apps.prefijo," ",inv_doc_encabezados.consecutivo) AS DOCUMENTO'),
+                'inv_bodegas.descripcion AS BODEGA',
+                DB::raw('CONCAT(core_terceros.descripcion," (",core_terceros.razon_social,")") AS TERCERO'),
+                'inv_doc_encabezados.descripcion AS DETALLE',
+                'inv_doc_encabezados.estado AS ESTADO'
+            ])
+            ->where(function ($query) use ($search) {
+                $query->where("inv_doc_encabezados.fecha", "LIKE", "%$search%")
+                    ->orWhere(DB::raw('CONCAT(core_tipos_docs_apps.prefijo," ",inv_doc_encabezados.consecutivo)'), "LIKE", "%$search%")
+                    ->orWhere("inv_bodegas.descripcion", "LIKE", "%$search%")
+                    ->orWhere(DB::raw('CONCAT(core_terceros.descripcion," (",core_terceros.razon_social,")")'), "LIKE", "%$search%")
+                    ->orWhere("inv_doc_encabezados.descripcion", "LIKE", "%$search%")
+                    ->orWhere("inv_doc_encabezados.estado", "LIKE", "%$search%");
+            })
+            ->orderBy('inv_doc_encabezados.created_at', 'DESC')
+            ->toSql();
+        return str_replace('?', '"%' . $search . '%"', $string);
+    }
 
-        $string = InvDocEncabezado::leftJoin('core_tipos_docs_apps', 'core_tipos_docs_apps.id', '=', 'inv_doc_encabezados.core_tipo_doc_app_id')
+    private static function queryConsultarRegistros($select = null)
+    {
+        $core_tipo_transaccion_id = 27; // Inventario Físico
+
+        $select = $select ?: [
+            DB::raw('DATE_FORMAT(inv_doc_encabezados.fecha,"%d-%m-%Y") AS campo1'),
+            DB::raw('CONCAT(core_tipos_docs_apps.prefijo," ",inv_doc_encabezados.consecutivo) AS campo2'),
+            'inv_bodegas.descripcion AS campo3',
+            DB::raw('CONCAT(core_terceros.descripcion," (",core_terceros.razon_social,")") AS campo4'),
+            'inv_doc_encabezados.descripcion AS campo5',
+            'inv_doc_encabezados.estado AS campo6',
+            'inv_doc_encabezados.id AS campo7'
+        ];
+
+        $query = InvFisico::leftJoin('core_tipos_docs_apps', 'core_tipos_docs_apps.id', '=', 'inv_doc_encabezados.core_tipo_doc_app_id')
             ->leftJoin('core_terceros', 'core_terceros.id', '=', 'inv_doc_encabezados.core_tercero_id')
             ->leftJoin('inv_bodegas', 'inv_bodegas.id', '=', 'inv_doc_encabezados.inv_bodega_id')
             ->where('inv_doc_encabezados.core_tipo_transaccion_id', $core_tipo_transaccion_id)
             ->where('inv_doc_encabezados.core_empresa_id', Auth::user()->empresa_id)
-            ->select(
-                DB::raw('DATE_FORMAT(inv_doc_encabezados.fecha,"%d-%m-%Y") AS FECHA'),
-                DB::raw( 'CONCAT(core_tipos_docs_apps.prefijo," ",inv_doc_encabezados.consecutivo) AS DOCUMENTO' ),
-                'inv_bodegas.descripcion AS BODEGA',
-                DB::raw( 'CONCAT(core_terceros.descripcion," (",core_terceros.razon_social,")") AS TERCERO' ),
-                'inv_doc_encabezados.descripcion AS DETALLE',
-                'inv_doc_encabezados.estado AS ESTADO'
-            )
-            ->where("inv_doc_encabezados.fecha", "LIKE", "%$search%")
-            ->orWhere(DB::raw('CONCAT(core_tipos_docs_apps.prefijo," ",inv_doc_encabezados.consecutivo)'), "LIKE", "%$search%")
-            ->orWhere("inv_bodegas.descripcion", "LIKE", "%$search%")
-            ->orWhere(DB::raw('CONCAT(core_terceros.descripcion," (",core_terceros.razon_social,")")'), "LIKE", "%$search%")
-            ->orWhere("inv_doc_encabezados.descripcion", "LIKE", "%$search%")
-            ->orWhere("inv_doc_encabezados.estado", "LIKE", "%$search%")
-            ->orderBy('inv_doc_encabezados.created_at', 'DESC')
-            ->toSql();
-        return str_replace('?', '"%' . $search . '%"', $string);
+            ->select($select);
+
+        return self::aplicarFiltroCreadoPor($query, 'inv_doc_encabezados.creado_por');
     }
 
     //Titulo para la exportación en PDF y EXCEL
